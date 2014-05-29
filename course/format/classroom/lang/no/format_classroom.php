3e496ad81c2fa8cb0245c5894a4f40b974e486ce
Efaktor AS <efaktor@efk-app02.osl.basefarm.net>
2014-05-29 15:32:16 +0200

    Added Norwegian translation

---
+++ course/format/classroom/lang/no/format_classroom.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_classroom', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_classroom
+ * @copyright  2014 eFaktor
+ * @author     Urs Hunkler {@link urs.hunkler@unodo.de}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['currentsection'] = 'Dette emnet';
+$string['sectionname'] = 'Emne';
+$string['pluginname'] = 'Klasseromsformat';
+$string['section0name'] = 'Generelt';
+$string['page-course-view-classroom'] = 'En kursforside i klasseromsformat';
+$string['page-course-view-classroom-x'] = 'En kursside i klasseromsformat';
+$string['hidefromothers'] = 'Skjul emnet';
+$string['showfromothers'] = 'Vis emnet';
+$string['forums'] = 'Oppslagstavle';
+$string['progress'] = 'Min progresjon';
+$string['lessonlastpageon'] = 'Vennligst velg neste aktivitet fra navigasjonsmenyen til venstre.';
+$string['lessonlastpageoff'] = 'Du er ferdig med denne leksjonen. Vennligst velg neste aktivitet i navigasjonsmenyen til venstre.';
