b2f567f830ca3c1c4737c6bce0459317415b4a74
Efaktor AS <efaktor@efk-app02.osl.basefarm.net>
2014-05-29 15:27:59 +0200

    Norwegian translation

---
+++ course/format/netcourse/lang/no/format_netcourse.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_netcourse', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_netcourse
+ * @copyright  2014 eFaktor
+ * @author     Urs Hunkler {@link urs.hunkler@unodo.de}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['currentsection'] = 'Dette emnet';
+$string['sectionname'] = 'Emne';
+$string['pluginname'] = 'Nettkursformat';
+$string['section0name'] = 'Generelt';
+$string['page-course-view-netcourse'] = 'En kursforside i nettkursformat';
+$string['page-course-view-netcourse-x'] = 'En kursside i nettkursformat';
+$string['hidefromothers'] = 'Skjul emnet';
+$string['showfromothers'] = 'Vis emnet';
+$string['forums'] = 'Forum';
+$string['progress'] = 'Min progresjon';
+$string['lessonlastpageon'] = 'Vennligs velg neste aktivitet fra navigasjonsmenyen til venstre.';
+$string['lessonlastpageoff'] = 'Du har fullført denne leksjonen. Vennligst velg neste aktivitet i navigasjonsmenyen til venstre.;
