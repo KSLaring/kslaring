00458c531285d24417522e9a73cf475ebb35f252
Efaktor AS <efaktor@efk-app01.osl.basefarm.net>
2014-06-11 10:38:55 +0200

    Added related courses plugin

---
+++ local/related_courses/lang/no/local_related_courses.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Related Courses (local)  - Language Settings (Norwegian)
+ *
+ * @package         local
+ * @subpackage      related_courses
+ * @copyright       2014    eFaktor {@link http://www.efaktor.no}
+ *
+ * @creationDate    24/04/2014
+ * @author          eFaktor     (fbv)
+ */
+
+$string['pluginname']   = 'Tilkoblede kurs';
+$string['title']        = 'Koble til nye kurs';
+
+$string['related_courses:manage'] = 'Koble til nye kurs';
+
+$string['header']       = 'Kursliste';
+$string['none']         = 'Ingen kurs tilgjengelige';
+$string['none_rel']     = 'Ingen tilkoblede kurs';
+$string['sel_courses']  = 'Valgte';
+$string['av_courses']   = 'Tilgjengelige';
+$string['btn_return']   = 'Gå tilbake til kurset';
