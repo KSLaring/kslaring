003820165c7d7fb44246fff09bb92245bbf981c3
Efaktor AS <efaktor@efk-app01.osl.basefarm.net>
2014-06-11 10:18:36 +0200

    Added autologin

---
+++ local/autologin/lang/no/local_autologin.php
@@ -0,0 +1,13 @@
+<?php
+
+$string['pluginname']           = 'Autologin';
+
+$string['firstname']            = 'Anonym';
+$string['lastname']             = 'Bruker';
+$string['city']                 = 'Sted';
+$string['countrycode']          = 'NO';
+$string['loginerrortext']       = 'Kurskoden er feil.';
+$string['createusererrortext'] = 'Moodle melder om en brukerfeil.';
+
+$string['leave_link']           = 'Return Link:';
+$string['leave_value']          = 'https://kurs.kommit.no';
