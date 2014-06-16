7a9c9dfa3c883766e9c0cfe4728e4362ec6b6ad0
Jon Jack <jon@efaktor.no>
2014-06-16 15:50:23 +0200

    change footer logo

--- theme/kommit/layout/columns3.php
+++ theme/kommit/layout/columns3.php
@@ -136,7 +136,7 @@ echo $OUTPUT->doctype() ?>
 
         <div class="column">
             <div class="footer-logo">
-                <img src="<?php echo $OUTPUT->pix_url('ks_footer_logo', 'theme'); ?>" width="71" height="35"/>
+                <img src="<?php echo $OUTPUT->pix_url('ks_footer_logo', 'theme'); ?>" width="278" height="45"/>
             </div>
         </div>
         <p>© 2014 KOMMUNESEKTORENS ORGANISASJON</p>
