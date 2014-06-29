4070ab59764f7671de01272d78bbf14c79f0d53e
Efaktor AS <efaktor@efk-app02.osl.basefarm.net>
2014-06-29 11:20:22 +0200

    Added the local plugin for the municipality block

---
+++ local/muni_block/lang/en/local_muni_block.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Local Municipality Block  - Language Strings (English)
+ *
+ * @package         local
+ * @subpackage      muni_block
+ * @copyright       2013 efaktor    {@link http://www.efaktor.no}
+ *
+ * @creationDate    22/08/2013
+ * @author          efaktor     (fbv)
+ */
+
+$string['pluginname']   = 'Municipality Block';
+
+
+$string['title']        = 'Municipality';
+$string['required']     = 'This field is required';
+$string['save']         = 'Save';
+$string['choose_muni']  = 'Choose Municipality ...';
+$string['sel_muni']     = 'Municipalities';
