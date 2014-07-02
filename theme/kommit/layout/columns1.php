eeb09958fc75593f3c00c6e9c9f1b40e30981232
Jon Jack <jon@efaktor.no>
2014-07-02 09:57:18 +0200

    add alt text to logo in footer

--- theme/kommit/layout/columns1.php
+++ theme/kommit/layout/columns1.php
@@ -104,7 +104,7 @@ echo $OUTPUT->doctype() ?>
 
         <div class="column">
             <div class="footer-logo">
-                <img src="<?php echo $OUTPUT->pix_url('ks_footer_logo', 'theme'); ?>" width="278" height="45"/>
+                <img src="<?php echo $OUTPUT->pix_url('ks_footer_logo', 'theme'); ?>" width="278" height="45" alt="ks footer logo">
             </div>
         </div>
         <p>© 2014 KOMMUNESEKTORENS ORGANISASJON</p>
