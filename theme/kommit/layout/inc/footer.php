33f89720975f5ab05a8d6c4e328b0018200b688d
uhunkler <urs.hunkler@unodo.de>
2014-11-07 10:22:48 +0100

    Add the Login link to the black bar in the page header - remove the link in the footer
    Related to-do: KommIT: Activate the Login-link in the header again (up right)
    https://basecamp.com/2124401/projects/6174826/todos/130232576#comment_216935074

--- theme/kommit/layout/inc/footer.php
+++ theme/kommit/layout/inc/footer.php
@@ -5,12 +5,6 @@
                 <img src="<?php echo $OUTPUT->pix_url('ks_footer_logo', 'theme'); ?>" width="278" height="45" alt="ks footer logo">
             </div>
             <p>© 2014 KOMMUNESEKTORENS ORGANISASJON</p>
-
-            <p>
-                <a href="<?php echo $CFG->wwwroot ?>//login/index.php">
-                    <?php echo(get_string('login')) ?>
-                </a>
-            </p>
         </div>
         <div class="footer-right">
             <div class="social">
