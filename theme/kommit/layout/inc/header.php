9e6fc289b069c18f882f7e6b9a1b3c63c8d90450
uhunkler <urs.hunkler@unodo.de>
2014-11-07 16:56:41 +0100

    Add a login link into the black barr in the page header

--- theme/kommit/layout/inc/header.php
+++ theme/kommit/layout/inc/header.php
@@ -1,39 +1,39 @@
+<?php // All PHP goes to the top
+/**
+ * @updateDate  09/09/2014
+ * @author      eFaktor     (fbv)
+ *
+ * Description
+ * Add the Municipality log to the header
+ */
+$muni = null;
+$logo = null;
+require_once($CFG->dirroot . '/blocks/municipality/municipalitylib.php');
+$loggedin = isloggedin();
+$loggedinclass = ' not-loggedin';
+if ($loggedin) {
+    /* Get the municipality connected with the user */
+    $muni = Municipality::municipality_ExitsMuni_User($USER->id);
+    /* Get the municipality logo */
+    $logo = Municipality::municipality_GetLogo($muni);
+    $loggedinclass = ' loggedin';
+}
+?>
+
 <div class="top-border">
 </div>
 
-<?php
-    /**
-     * @updateDate  09/09/2014
-     * @author      eFaktor     (fbv)
-     *
-     * Description
-     * Add the Municipality log to the header
-     */
-    $muni = null;
-    $logo = null;
-    require_once($CFG->dirroot . '/blocks/municipality/municipalitylib.php');
-    if (isloggedin()) {
-        /* Get the municipality connected with the user */
-        $muni = Municipality::municipality_ExitsMuni_User($USER->id);
-        /* Get the municipality logo */
-        $logo = Municipality::municipality_GetLogo($muni);
-    }
-?>
 
 <div class="header-background">
     <div class="container-fluid">
         <div class="logo-area">
-            <a class="logo" href="<?php echo $CFG->wwwroot;?>"><img class="logo" alt="kommit logo" src="<?php echo
-                $OUTPUT->pix_url('logo', 'theme'); ?>"></a>
-
-
-
+            <a class="logo" href="<?php echo $CFG->wwwroot; ?>"><img class="logo"
+                                                                     alt="kommit logo"
+                                                                     src="<?php echo
+                                                                     $OUTPUT->pix_url('logo', 'theme'); ?>"></a>
         </div>
 
-
-
         <div class="header-right">
-
             <?php
             /**
              * @updateDate  09/09/2014
@@ -46,18 +46,19 @@
                 echo '<div class="muni-logo">' . $logo . '</div>';
             }
             ?>
-
         </div>
 
     </div>
 </div>
 
-<div id="header" class="header">
-    <header role="banner" class="navbar navbar-fixed-top<?php echo $html->navbarclass ?> moodle-has-zindex">
+<div id="header" class="header<?php echo $loggedinclass; ?>">
+    <header role="banner"
+            class="navbar navbar-fixed-top<?php echo $html->navbarclass ?> moodle-has-zindex">
         <nav role="navigation" class="navbar-inner">
             <div class="container-fluid">
 
-                <a class="btn btn-navbar" data-toggle="workaround-collapse" data-target=".nav-collapse">
+                <a class="btn btn-navbar" data-toggle="workaround-collapse"
+                   data-target=".nav-collapse">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -66,8 +67,11 @@
                 <div id="moodle-navbar" class="nav-collapse collapse">
                     <?php echo $OUTPUT->custom_menu(); ?>
                     <?php echo $OUTPUT->user_menu(); ?>
-
                 </div>
+
+                <?php if (!$loggedin) : ?>
+                <div class="navbar-text"><?php echo $OUTPUT->login_info() ?></div>
+                <?php endif ?>
             </div>
         </nav>
     </header>
