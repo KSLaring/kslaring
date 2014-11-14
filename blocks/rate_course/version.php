329f01bfc75e7222a8eefd87443900d452f66f41
Paqui Bellés Vallejo <fbv@efaktor.no>
2014-11-14 10:23:51 +0100

    Moodle_27_Stable_Dev
    Block Course Rates
    
    Signed-off-by: Paqui Bellés Vallejo <fbv@efaktor.no>

--- blocks/rate_course/version.php
+++ blocks/rate_course/version.php
@@ -27,7 +27,7 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
  */
 
-$plugin->version      = 2014090300;
+$plugin->version      = 2014111400;
 
 $plugin->requires     = 2013040501;
 $plugin->maturity     = MATURITY_STABLE;
