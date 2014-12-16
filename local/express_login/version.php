388791c2497bea62bcf8d255244e529d83263a3d
Paqui Bellés Vallejo <fbv@efaktor.no>
2014-12-16 14:58:36 +0100

    Moodle_27_STable
    Express Login

--- local/express_login/version.php
+++ local/express_login/version.php
@@ -12,6 +12,6 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version    = 2014121100;                   /* The current plugin version (Date: YYYYMMDDXX)  */
+$plugin->version    = 2014121600;                   /* The current plugin version (Date: YYYYMMDDXX)  */
 //$plugin->requires = 2012061700;                   /* Requires this Moodle version                   */
 $plugin->component  = 'local_express_login';        /* Full name of the plugin (used for diagnostics) */
