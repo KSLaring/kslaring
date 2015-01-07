31613b9801ca646b7177be90c0a7ff8aa165d2a5
Efaktor AS <efaktor@efk-mdl01.osl.basefarm.net>
2015-01-07 15:14:32 +0100

    Updated logos

---
+++ auth/saml/saml/saml_config.php
@@ -0,0 +1,1 @@
+{"samllib":"\/www\/efaktor\/simplesamlphp\/kurs.kommit.no\/lib","sp_source":"idporten","dosinglelogout":false}
---
+++ auth/saml/saml/saml_config_idporten.difi.no-v2.php
@@ -0,0 +1,1 @@
+{"samllib":"\/www\/efaktor\/simplesamlphp\/kurs.kommit.no\/lib","sp_source":"idporten.difi.no-v2","dosinglelogout":true}
