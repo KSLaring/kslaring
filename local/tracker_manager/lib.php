141fbe52d46fceeb8f28c4336736ae3da083e5cc
Alf Martin Johnsen <amj@efaktor.no>
2015-02-26 20:39:55 +0100

    Better menu sort order

--- local/tracker_manager/lib.php
+++ local/tracker_manager/lib.php
@@ -8,12 +8,12 @@ function local_tracker_manager_extends_navigation(global_navigation $navigation)
 
         if (is_siteadmin($USER->id)) {
             $nodBar = $nodeTracker->add(get_string('report_manager','local_tracker_manager'),new moodle_url('/report/manager/index.php'));
-            $nodBar = $nodeTracker->add(get_string('company_structure','local_tracker_manager'),new moodle_url('/report/manager/company_structure/company_structure.php'));
             $nodBar = $nodeTracker->add(get_string('outcome_report','local_tracker_manager'),new moodle_url('/report/manager/outcome_report/outcome_report.php'));
             $nodBar = $nodeTracker->add(get_string('course_report','local_tracker_manager'),new moodle_url('/report/manager/course_report/course_report.php'));
+            $nodBar = $nodeTracker->add(get_string('outcome_area_title','local_tracker_manager'),new moodle_url('/grade/edit/outcome/index.php'));
+			$nodBar = $nodeTracker->add(get_string('company_structure','local_tracker_manager'),new moodle_url('/report/manager/company_structure/company_structure.php'));
             $nodBar = $nodeTracker->add(get_string('job_roles','local_tracker_manager'),new moodle_url('/report/manager/job_role/job_role.php'));
             $nodBar = $nodeTracker->add(get_string('outcomes','local_tracker_manager'),new moodle_url('/report/manager/outcome/outcome.php'));
-            $nodBar = $nodeTracker->add(get_string('outcome_area_title','local_tracker_manager'),new moodle_url('/grade/edit/outcome/index.php'));
         }else {
             if (has_capability('report/manager:viewlevel4', CONTEXT_SYSTEM::instance())) {
                 $nodBar = $nodeTracker->add(get_string('report_manager','local_tracker_manager'),new moodle_url('/report/manager/index.php'));
