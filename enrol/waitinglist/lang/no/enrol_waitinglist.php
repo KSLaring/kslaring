867b22dc73608d38916d3981ebafe8572d49e30d
Alf Martin Johnsen <amj@efaktor.no>
2015-03-30 07:49:22 +0200

    Some more translations

--- enrol/waitinglist/lang/no/enrol_waitinglist.php
+++ enrol/waitinglist/lang/no/enrol_waitinglist.php
@@ -129,5 +129,17 @@ Følgende plassholdere kan legges inn i meldingen:
 $string['sendcoursewaitlistmessage'] = 'Send epost når brukeren blir lagt til i ventelisten';
 $string['sendcoursewaitlistmessage_help'] = 'Du kan sende en epost til brukeren når de legges til på kursets venteliste.';
 
-
-
+$string['manageconfirmed'] = 'Bekreftede påmeldinger';
+$string['unconfirmfailed'] = 'Fjerning fra bekreftede påmeldinger mislykkes!';
+$string['waitinglistisempty'] = 'Ventelisten er tom';
+$string['confirmedlistisempty'] = 'Listen med bekreftede påmeldinger er tom';
+$string['unconfirm'] = 'Fjern bekreftelse';
+$string['unconfirmwarning'] = 'Ønsker du virkelig å returnere disse kursplassene til ventelisten?';
+$string['noroomonlist'] = 'Vi beklager, men ventelisten er full.';
+$string['enrolmentsnotyet'] = 'Påmelding med venteliste er ikke åpnet ennå.';
+$string['enrolmentsclosed'] = 'Påmelding med venteliste er nå stengt.';
+$string['alreadyenroled'] = 'Du er allerede påmeldt dette kurset.';
+$string['qentrynothingchanged'] = 'Det trengs ingen oppdatering av dine påmeldinger.';
+$string['onlyoneenrolmethodallowed'] = '';//'You can only be on the waitinglist once.'; //strange to show this to user
+$string['nomoreseats'] = 'Det ønskede antall kursplasser er ikke tilgjengelig, men det er fortsatt {$a->available} ledige plasser på ventelisten.';
+$string['entercoursenow'] = 'Fortsett til kurset';
