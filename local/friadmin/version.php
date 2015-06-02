79fd1bc7e808b919dadbde681d4e0b970b5d2a0d
uhunkler <urs.hunkler@unodo.de>
2015-06-02 17:12:58 +0200

    local_friadmin - updated version number

--- local/friadmin/version.php
+++ local/friadmin/version.php
@@ -26,6 +26,6 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2015060200;         /* The current plugin version (Date: YYYYMMDDXX)  */
+$plugin->version = 2015060201;         /* The current plugin version (Date: YYYYMMDDXX)  */
 $plugin->requires = 2014051205;        /* Requires this Moodle version                   */
 $plugin->component = 'local_friadmin'; /* Full name of the plugin (used for diagnostics) */
