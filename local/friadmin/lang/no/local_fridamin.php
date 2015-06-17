256979c6128474ed55946f35b38064b78ad02d23
Alf Martin Johnsen <amj@efaktor.no>
2015-06-17 09:00:26 +0200

    Translations

--- local/friadmin/lang/no/local_fridamin.php
+++ local/friadmin/lang/no/local_fridamin.php
@@ -17,129 +17,129 @@
 
 $string['pluginname']       = 'Friadmin';
 
-$string['courselist_title']     = 'Friadmin available courses';
-$string['coursetable_title']    = 'Course table';
-$string['course_courseid']      = 'Course ID';
-$string['course_name']          = 'Name';
-$string['course_summary']       = 'Summary';
-$string['course_targetgroup']   = 'Target group';
-$string['course_date']          = 'Date';
-$string['course_time']          = 'Time from - to';
-$string['course_seats']         = 'Avail.&nbsp;seats';
-$string['course_deadline']      = 'Deadline';
-$string['course_length']        = 'Length';
-$string['course_municipality']  = 'Municipality';
-$string['course_sector']        = 'Sector';
-$string['course_location']      = 'Location';
-$string['course_responsible']   = 'In charge';
-$string['course_teacher']       = 'Teacher';
-$string['course_priceinternal'] = 'Price internal';
-$string['course_priceexternal'] = 'Price external';
-$string['course_link']          = 'Link';
+$string['courselist_title']     = 'Friadmin tilgjengelige kurs';
+$string['coursetable_title']    = 'Kursoversikt';
+$string['course_courseid']      = 'Kurs ID';
+$string['course_name']          = 'Navn';
+$string['course_summary']       = 'Beskrivelse';
+$string['course_targetgroup']   = 'Målgruppe';
+$string['course_date']          = 'Dato';
+$string['course_time']          = 'Tid fra - til';
+$string['course_seats']         = 'Tilgjengelige plasser';
+$string['course_deadline']      = 'Påmeldingsfrist';
+$string['course_length']        = 'Varighet';
+$string['course_municipality']  = 'Kommune';
+$string['course_sector']        = 'Sektor';
+$string['course_location']      = 'Kurssted';
+$string['course_responsible']   = 'Ansvarlig';
+$string['course_teacher']       = 'Kurslærer';
+$string['course_priceinternal'] = 'Internpris';
+$string['course_priceexternal'] = 'Eksternpris';
+$string['course_link']          = 'Lenke';
 $string['course_edit']          = '';
 
-$string['coursedetail_title']       = 'Friadmin course details';
-$string['coursedetail_back']        = 'Back to courselist';
-$string['coursedetail_users']       = 'Enrolled users';
-$string['coursedetail_waitlist']    = 'Manage queue';
-$string['coursedetail_confirmed']   = 'Manage confirmed';
-$string['coursedetail_settings']    = 'Course settings';
-$string['coursedetail_go']          = 'Open course';
-$string['coursedetail_duplicate']   = 'Duplicate';
-$string['coursedetail_email']       = 'Send email';
+$string['coursedetail_title']       = 'Friadmin kursdetaljer';
+$string['coursedetail_back']        = 'Tilbake til kursoversikten';
+$string['coursedetail_users']       = 'Påmeldte kursdeltakere';
+$string['coursedetail_waitlist']    = 'Administrere venteliste';
+$string['coursedetail_confirmed']   = 'Administrere bekreftede';
+$string['coursedetail_settings']    = 'Kursinnstillinger';
+$string['coursedetail_go']          = 'Gå til kurset';
+$string['coursedetail_duplicate']   = 'Dupliser';
+$string['coursedetail_email']       = 'Send epost';
 
-$string['coursetemplate_title']         = 'Friadmin add from template';
-$string['coursetemplate_subtitle']      = 'Create a course from a course template.';
-$string['coursetemplate_cat']           = 'Course template category';
-$string['coursetemplate_cat_desc']      = 'Please select the course category where all template courses are stored.';
-$string['coursetemplate_cat_select']    = 'Select course template category ...';
-$string['coursetemplate_go']            = 'Open course';
-$string['coursetemplate_another']       = 'Create another course';
-$string['coursetemplate_settings']      = 'Course settings';
-$string['coursetemplate_overview']      = 'Course overview';
-$string['coursetemplate_result']        = 'The course has been created -
-id: <strong>{$a->id}</strong>, shortname: "<strong>{$a->shortname}</strong>",
-fullname: "<strong>{$a->fullname}</strong>".';
-$string['coursetemplate_error']         = 'Course could not be created.';
+$string['coursetemplate_title']         = 'Friadmin - Legg til fra kursmal';
+$string['coursetemplate_subtitle']      = 'Opprett et kurs fra en kursmal.';
+$string['coursetemplate_cat']           = 'Kurskategori for kursmaler';
+$string['coursetemplate_cat_desc']      = 'Vennligst velg kurskategorien hvor alle kursmaler skal lagres.';
+$string['coursetemplate_cat_select']    = 'Velg kurskategori for kursmaler ...';
+$string['coursetemplate_go']            = 'Gå til kurset';
+$string['coursetemplate_another']       = 'Opprett enda et kurs';
+$string['coursetemplate_settings']      = 'Kursinnstillinger';
+$string['coursetemplate_overview']      = 'Kursmaloversikt';
+$string['coursetemplate_result']        = 'Kurset er opprettet -
+id: <strong>{$a->id}</strong>, kortnavn: "<strong>{$a->shortname}</strong>",
+Fullt navn: "<strong>{$a->fullname}</strong>".';
+$string['coursetemplate_error']         = 'Kurset kunne ikke opprettes.';
 
-$string['location']                     = 'Location: ';
-$string['fromto']                       = 'From - to: ';
-$string['coursename']                   = 'Course name: ';
-$string['selmunicipality']              = 'My municipalities';
-$string['selsector']                    = 'All sectors';
-$string['sellocation']                  = 'All locations';
-$string['selname']                      = 'Course name';
-$string['selcategory']                  = 'Target category';
-$string['missingselcategory']           = 'Missing target category';
-$string['seltemplate']                  = 'Course template';
-$string['missingseltemplate']           = 'Missing course template';
-$string['selsubmit']                    = 'Search';
-$string['selsubmitcreate']              = 'Create course';
+$string['location']                     = 'Sted: ';
+$string['fromto']                       = 'Fra - til: ';
+$string['coursename']                   = 'Kursnavn: ';
+$string['selmunicipality']              = 'Søk overalt';
+$string['selsector']                    = 'Alle sektorer';
+$string['sellocation']                  = 'Alle kurssteder';
+$string['selname']                      = 'Kursnavn';
+$string['selcategory']                  = 'Lagres i kategorien';
+$string['missingselcategory']           = 'Mangler målkategori';
+$string['seltemplate']                  = 'Kursmal';
+$string['missingseltemplate']           = 'Mangler kursmal';
+$string['selsubmit']                    = 'Søk';
+$string['selsubmitcreate']              = 'Opprett kurs';
 
-$string['edit'] = 'Edit course';
-$string['show'] = 'Show details';
+$string['edit'] = 'Endre kurs';
+$string['show'] = 'Vis detaljer';
 
 /* ********************** */
 /* Course Location Plugin */
 /* ********************** */
-$string['plugin_course_locations']               = 'Course Locations';
-$string['friadmin:course_locations_manage']      = 'Manage Course Locations';
+$string['plugin_course_locations']               = 'Kurssteder';
+$string['friadmin:course_locations_manage']      = 'Administrere kurssteder';
 
-$string['lst_locations']        = 'Browse list of locations';
-$string['new_location']         = 'New Location';
-$string['edit_location']        = 'Edit Location';
-$string['edit']                 = 'Edit';
-$string['del_location']         = 'Delete Location';
-$string['view_location']        = 'View Location';
-$string['courses_locations']    = 'Courses Location List';
+$string['lst_locations']        = 'Vis liste over kurssteder';
+$string['new_location']         = 'Nytt kurssted';
+$string['edit_location']        = 'Rediger kurssted';
+$string['edit']                 = 'Endre';
+$string['del_location']         = 'Slett kurssted';
+$string['view_location']        = 'Vis kurssted';
+$string['courses_locations']    = 'Liste over kurssteder';
 
-$string['title_locations']          = 'Locations';
-$string['title_courses_locations']  = 'Available Courses';
-$string['title_general']            = 'General';
+$string['title_locations']          = 'Kurssteder';
+$string['title_courses_locations']  = 'Tilgjengelige kurs';
+$string['title_general']            = 'Generelt';
 
-$string['exist_locations']          = 'Existing Locations';
-$string['location']                 = 'Location';
+$string['exist_locations']          = 'Eksisterende kurssteder';
+$string['location']                 = 'Kurssted';
 $string['filter']                   = 'Filter';
-$string['municipality']             = 'Municipalities';
-$string['counties']                 = 'Counties';
-$string['sectors']                  = 'Sectors';
-$string['select_level_list']        = 'Select Item';
-$string['activate']                 = 'Activate';
-$string['deactivate']               = 'Deactivate';
+$string['municipality']             = 'Kommuner';
+$string['counties']                 = 'Fylker';
+$string['sectors']                  = 'Sektorer';
+$string['select_level_list']        = 'Velg element';
+$string['activate']                 = 'Aktiver';
+$string['deactivate']               = 'Deaktiver';
 
-$string['location_county']      = 'County';
-$string['location_muni']        = 'Municipality';
-$string['location_name']        = 'Name';
-$string['location_desc']        = 'Description';
-$string['location_url']         = 'Url more information';
-$string['location_floor']       = 'Floor';
-$string['location_room']        = 'Room';
-$string['location_seats']       = 'Max. Seats';
-$string['location_detail']      = 'Detail';
-$string['location_address']     = 'Address';
-$string['location_street']      = 'Street';
-$string['location_post_code']   = 'Post Code';
-$string['location_city']        = 'City';
-$string['location_map']         = 'Url map';
-$string['location_post']        = 'Post address';
-$string['location_contact']     = 'Contact person';
-$string['location_phone']       = 'Contact phone';
-$string['location_mail']        = 'Contact eMail';
-$string['location_contact_inf'] = 'Contact';
-$string['location_comments']    = 'Comments';
+$string['location_county']      = 'Fylke';
+$string['location_muni']        = 'Kommune';
+$string['location_name']        = 'Navn';
+$string['location_desc']        = 'Beskrivelse';
+$string['location_url']         = 'URL til mer informasjon';
+$string['location_floor']       = 'Etasje';
+$string['location_room']        = 'Rom';
+$string['location_seats']       = 'Maks antall plasser';
+$string['location_detail']      = 'Detaljer';
+$string['location_address']     = 'Adresse';
+$string['location_street']      = 'Gate';
+$string['location_post_code']   = 'Postnmmer';
+$string['location_city']        = 'Sted';
+$string['location_map']         = 'URL til kart';
+$string['location_post']        = 'Postadresse';
+$string['location_contact']     = 'Kontaktperson';
+$string['location_phone']       = 'Kontakt-tlf';
+$string['location_mail']        = 'Komtakt epost';
+$string['location_contact_inf'] = 'Kontakt';
+$string['location_comments']    = 'Kommentarer';
 
-$string['sel_location']         = 'Select location ...';
-$string['sel_sector']           = 'Select sector ...';
+$string['sel_location']         = 'Velg kurssted ...';
+$string['sel_sector']           = 'Velg sektor ...';
 
-$string['no_data']                  = 'No data found for your selection.';
-$string['return_to_selection']      = 'Return to locations selection page';
+$string['no_data']                  = 'Fant ingen resultater for utvalget ditt.';
+$string['return_to_selection']      = 'Tilbake til siden for kurssteder';
 
-$string['error_deleting_location']          = 'Sorry, the location could not be deleted because there are courses connected with';
-$string['deleted_location']                 = 'The location has been revomed';
-$string['delete_location_are_you_sure']     = '<p>Are you sure you want to delete the next location ? </p>
-<li><strong>Municipality: </strong>{$a->muni}</li>
-<li><strong>Location: </strong>{$a->name}</li>
-<li><strong>Address: </strong>{$a->address}</li>';
+$string['error_deleting_location']          = 'Beklager, men kursstedet kunne ikke slettes fordi det er i bruk i ett eller flere kurs.';
+$string['deleted_location']                 = 'Kursstedet er slettet';
+$string['delete_location_are_you_sure']     = '<p>Er du sikker på at du vil slette kursstedet?</p>
+<li><strong>Kommune: </strong>{$a->muni}</li>
+<li><strong>Kurssted: </strong>{$a->name}</li>
+<li><strong>Adresse: </strong>{$a->address}</li>';
 
-$string['btn_save']     = 'Save';
-$string['lnk_back']     = 'Back';
+$string['btn_save']     = 'Lagre';
+$string['lnk_back']     = 'Tilbake';
