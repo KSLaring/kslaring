fa9114cb0f10bf8c9667d7eadd21234950bb9116
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-06-18 14:05:36 +0200

    FIRST ACCESS - MOODLE_27_STABLE
    It only shows the fields to update

--- local/first_access/lang/no/local_first_access.php
+++ local/first_access/lang/no/local_first_access.php
@@ -12,8 +12,12 @@
  * @author          eFaktor     (fbv)
  *
  */
-$string['pluginname']   = 'Profilfullføring';
+$string['pluginname']       = 'Profilfullføring';
 
 $string['welcome_title']    = 'Velkommen som ny bruker på KS læring';
 $string['welcome_message']  = 'Før vi starter måa vi be deg om å fullføre din personlige brukerprofil. Det tar bare et par minutter og er viktig for at vi skal få registrert deg med riktig personalia, kommune, jobbroller du måtte ha osv.';
 $string['welcome_btn']      = 'Fullfør min personlige brukerprofil';
+
+$string['header_muni']      = 'Kommune';
+
+$string['btn_save']         = 'Save';
