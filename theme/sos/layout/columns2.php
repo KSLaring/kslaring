8888c4b2f4e39e9b3489aa0ff09302936f66ec3f
uhunkler <urs.hunkler@unodo.de>
2015-06-19 09:59:51 +0200

    theme_sos - add the sos theme for testing

---
+++ theme/sos/layout/columns2.php
@@ -0,0 +1,113 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+// Get the HTML for the settings bits.
+$html = theme_sos_get_html_for_settings($OUTPUT, $PAGE);
+
+$left = (!right_to_left());  // To know if to add 'pull-right' and 'desktop-first-column' classes in the layout for LTR.
+echo $OUTPUT->doctype() ?>
+<html <?php echo $OUTPUT->htmlattributes(); ?>>
+<head>
+    <title><?php echo $OUTPUT->page_title(); ?></title>
+    <link rel="shortcut icon" href="<?php echo $OUTPUT->favicon(); ?>" />
+    <?php echo $OUTPUT->standard_head_html() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body <?php echo $OUTPUT->body_attributes('two-column'); ?>>
+
+<?php echo $OUTPUT->standard_top_of_body_html() ?>
+
+<header role="banner" class="navbar navbar-fixed-top<?php echo $html->navbarclass ?> moodle-has-zindex">
+    <nav role="navigation" class="navbar-inner">
+        <div class="container-fluid">
+            <a class="brand" href="<?php echo $CFG->wwwroot;?>"><?php echo $SITE->shortname; ?></a>
+            <a class="btn btn-navbar" data-toggle="workaround-collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <div class="nav-collapse collapse">
+                <?php echo $OUTPUT->custom_menu(); ?>
+                <ul class="nav pull-right">
+                    <li><?php echo $OUTPUT->page_heading_menu(); ?></li>
+                    <li class="navbar-text"><?php echo $OUTPUT->login_info() ?></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<header id="page-header" class="clearfix">
+
+    <div id="banner">
+    </div>
+</header>
+
+<div id="page-navbar" class="clearfix">
+    <div class="page-navbar-inner">
+        <div class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></div>
+        <nav class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></nav>
+    </div>
+</div>
+
+
+<?php // echo $html->heading; ?>
+
+<div id="course-header">
+    <?php echo $OUTPUT->course_header(); ?>
+</div>
+
+
+
+<div id="page" class="container-fluid">
+
+    <div id="page-content" class="row-fluid">
+        <section id="region-main" class="span9<?php if ($left) { echo ' pull-right'; } ?>">
+            <?php
+            echo $OUTPUT->course_content_header();
+            echo $OUTPUT->main_content();
+            echo $OUTPUT->course_content_footer();
+            ?>
+        </section>
+        <?php
+        $classextra = '';
+        if ($left) {
+            $classextra = ' desktop-first-column';
+        }
+        echo $OUTPUT->blocks('side-pre', 'span3'.$classextra);
+        ?>
+    </div>
+
+</div>
+
+<footer id="page-footer">
+    <div id="page-footer-inner" class="wrapper clearfix">
+
+        <div class="column0">
+            <?php echo $OUTPUT->login_info(); ?>
+            <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
+            <?php echo $OUTPUT->standard_footer_html(); ?>
+            <?php echo $OUTPUT->home_link(); ?>
+        </div>
+    </div>
+</footer>
+
+<?php echo $OUTPUT->standard_end_of_body_html() ?>
+
+
+</body>
+</html>
