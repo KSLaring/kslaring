72437c42185ed459ba4fddda438bd03298ab9524
memetoec <memetoec@gmail.com>
2015-08-05 14:38:09 +0200

    added theme klass

---
+++ theme/klass/renderers.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package    theme_academi
+ * @copyright  2015 onwards Nephzat Dev Team (http://www.nephzat.com)
+ * @authors    Nephzat Dev Team , nephzat.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once('renderers/course_renderer.php');
