cd6f541eb69eb1063b985f7c57ab3a22885c7823
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-09-08 09:48:19 +0200

    Microlearning

--- local/microlearning/lib.php
+++ local/microlearning/lib.php
@@ -79,12 +79,12 @@ function local_microlearning_cron() {
             /* Check if has to be run it    */
             if (isset($pluginInfo->lastcron)) {
                 /* Calculate when it has to be triggered it */
-                $timeYesterday  = mktime($cronHour - 3600, $cronMin - 60 , 0, $date['mon'], $date['mday'] - 1, $date['year']);
+                //$timeYesterday  = mktime($cronHour, $cronMin , 0, $date['mon'], $date['mday'] - 1, $date['year']);
 
-                if (($pluginInfo->lastcron <= $timeYesterday)) {
+                //if (($pluginInfo->lastcron <= $timeYesterday)) {
                     Calendar_ModeCron::cron();
                     Activity_ModeCron::cron();
-                }
+                //}
             }else {
                 Calendar_ModeCron::cron();
                 Activity_ModeCron::cron();
