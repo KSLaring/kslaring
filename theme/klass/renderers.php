4888df472cb5a85677e5d409e385e45f8d9fb68d
memetoec <memetoec@gmail.com>
2015-09-21 10:40:36 +0200

    Created branch for feide.weblogins with mdl ver. 2.9

--- theme/klass/renderers.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package    theme_academi
- * @copyright  2015 onwards Nephzat Dev Team (http://www.nephzat.com)
- * @authors    Nephzat Dev Team , nephzat.com
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-require_once('renderers/course_renderer.php');
