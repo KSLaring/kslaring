26f850a36c83fb2d1e247e5d5d54589db08897a4
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-09-28 13:30:09 +0200

    Login Via Feide

--- local/wsks/feide/feidelib.php
+++ local/wsks/feide/feidelib.php
@@ -125,6 +125,7 @@ class KS_FEIDE {
             /* Build end Point Service  */
             $server     = $domain . '/webservice/soap/server.php?wsdl=1&wstoken=' . $token;
 
+            echo "SErver : " . $server . "</br>";
             /* Call service */
             $client     = new SoapClient($server);
             $response   = $client->$service($userRequest);
