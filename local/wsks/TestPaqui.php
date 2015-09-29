1f17d955bb3c8e1a4d6941370947eb7eb73482bb
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-09-29 10:50:51 +0200

    TEsting Service

---
+++ local/wsks/TestPaqui.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: paqui
+ * Date: 29/09/15
+ * Time: 10:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+require_once('../../config.php');
+
+$url = new moodle_url('/local/wsks/TestPaqui.php');
+$PAGE->set_url($url);
+$PAGE->set_context(CONTEXT_SYSTEM::instance());
+$PAGE->set_pagelayout('admin');
+
+
+$token = '707b497d0b53021b64db79698afc26e5';
+$domain = 'https://feidedev.weblogin.no';
+
+$service = 'HelloWorld';
+
+$server_url     = $domain . '/webservice/soap/server.php?wsdl=1&wstoken=' . $token;
+
+echo $OUTPUT->header();
+
+try {
+    $client = new SoapClient($server_url);
+
+    $message = array();
+    $message['msg']         = 'Perico de los Palotes';
+
+    $resp = $client->$service($message);
+
+
+    if ($resp['error'] == '200') {
+        echo " OK -- > " . $resp['MyMessage'];
+    }else {
+        print($resp['error'] . '-' . $resp['msg_error']);
+    }
+}catch (Exception $ex) {
+    throw $ex;
+}//try_catch
+
+echo $OUTPUT->footer();
