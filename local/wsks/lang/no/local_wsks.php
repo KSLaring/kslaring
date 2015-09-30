5d8bb17ea2ddc20a7ecc20a451fd11132904aa4f
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-09-30 11:26:38 +0200

    WSKS Plugin

---
+++ local/wsks/lang/no/local_wsks.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * KS Læring Integration - Language Strings (Norwegian)
+ *
+ * @package         local
+ * @subpackage      wsks/lang
+ * @copyright       2015 eFaktor    {@link http://www.efaktor.no}
+ *
+ * @creationDate    22/09/2015
+ * @author          eFaktor     (fbv)
+ */
+
+$string['pluginname']   = 'KS Læring Integration';
+
+$string['feide_settings']   = 'Feide Settings';
+$string['feide_site']       = 'Feide Site';
+$string['feide_site_desc']  = 'Log In Via Feide';
+$string['feide_service']    = 'Feide Service';
+$string['feide_token']      = 'Feide Token';
+
+$string['adfs_settings']    = 'ADFS Settings';
+$string['adfs_site']        = 'ADFS Site';
+$string['adfs_site_desc']   = 'Log In Via ADFS';
+$string['adfs_service']     = 'ADFS Service';
+$string['adfs_token']       = 'ADFS Token';
+
+$string['FEIDE_ERR_NOT_VALID']  = 'User not valid. Please, contact your Moodle Administrator.';
+$string['FEIDE_ERR_PROCESS']    = 'There has been an error during the process. Please, contact your Moodle Administrator.';
+
