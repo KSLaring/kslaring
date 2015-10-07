324202adcc03b1678c8fffb61891606b67548995
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-10-07 12:40:53 +0200

    Remove Web Service Testing

--- local/wstest/lang/en/local_wstest.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: paqui
- * Date: 29/09/15
- * Time: 10:20
- * To change this template use File | Settings | File Templates.
- */
-$string['pluginname'] = 'Test';
