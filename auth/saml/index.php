e003cd80e926b48e14da0dab671b92cd56de8ab9
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-10-13 13:50:05 +0200

    Log In/Out

--- auth/saml/index.php
+++ auth/saml/index.php
@@ -36,7 +36,7 @@ define('SAML_INTERNAL', 1);
                 $urltogo = $_SERVER['HTTP_REFERER'];
             }
             else{
-                $urltogo = '/';
+                $urltogo = 'http://www.elpuntavui.cat';
             }
 
             if ($saml_param->dosinglelogout) {
