2a90a98679259409847a97cc910fae66b9ff7cee
Alf Martin Johnsen <amj@efaktor.no>
2015-10-15 15:38:52 +0200

    Translations

--- local/wsks/lang/no/local_wsks.php
+++ local/wsks/lang/no/local_wsks.php
@@ -10,20 +10,13 @@
  * @author          eFaktor     (fbv)
  */
 
-$string['pluginname']   = 'Feide Integration';
+$string['pluginname']   = 'Feide-integrasjon';
 
-$string['feide_settings']   = 'Feide Settings';
-$string['feide_site']       = 'Feide Site';
-$string['feide_site_desc']  = 'Log In Via Feide';
+$string['feide_settings']   = 'Feide-innstillinger';
+$string['feide_site']       = 'Feide login-server';
+$string['feide_site_desc']  = 'Logg inn via Feide';
 $string['feide_service']    = 'Feide Service';
 $string['feide_token']      = 'Feide Token';
-
-$string['adfs_settings']    = 'ADFS Settings';
-$string['adfs_site']        = 'ADFS Site';
-$string['adfs_site_desc']   = 'Log In Via ADFS';
-$string['adfs_service']     = 'ADFS Service';
-$string['adfs_token']       = 'ADFS Token';
-
-$string['FEIDE_ERR_NOT_VALID']  = 'User not valid. Please, contact your Moodle Administrator.';
-$string['FEIDE_ERR_PROCESS']    = 'There has been an error during the process. Please, contact your Moodle Administrator.';
+$string['FEIDE_ERR_NOT_VALID']  = 'Dette er ikke en gyldig bruker, vennligst kontakt administrator.';
+$string['FEIDE_ERR_PROCESS']    = 'Det har skjedd en feil under innloggingen. Vennligst kontakt administrator.';
 
