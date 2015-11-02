189bb2535536443a0d392f86d75194fbd434530a
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-11-02 09:42:06 +0100

    ADFS - KS Integration

---
+++ local/adfs/version.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ADFS Integration WebService - Version
+ *
+ * @package         local
+ * @subpackage      adfs
+ * @copyright       2015 eFaktor    {@link http://www.efaktor.no}
+ *
+ * @creationDate    31/10/2015
+ * @author          eFaktor     (fbv)
+ *
+ */
+
+$plugin->version    =   '2015103100';         /* The plugin current version (DATE: YYYYMMDDXX) */
+$plugin->component  =   'local_adfs';
