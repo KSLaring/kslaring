e7271d205b68394f46cdc6f8b1e8b1604f078902
Paqui Bellés Vallejo <fbv@efaktor.no>
2015-11-10 10:10:49 +0100

    Testing

--- auth/saml/index.php
+++ auth/saml/index.php
@@ -227,9 +227,9 @@ define('SAML_INTERNAL', 1);
             try {
                 $urlKS = KS_ADFS::LogIn_UserADFS($USER->id);
 
-                //header('Location: ' . urldecode($urlKS));
-                //require_logout();
-                //die;
+                header('Location: ' . urldecode($urlKS));
+                require_logout();
+                die;
             //    redirect($urltogo);
             }catch (Exception $ex) {
                 throw $ex;
