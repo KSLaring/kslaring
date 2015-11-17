ce509ce8fd687020fb44dd80fec7a4f2e740f00a
uhunkler <urs.hunkler@unodo.de>
2015-11-17 09:31:50 +0100

    theme_bergen - Add the bergen theme as a kommit child theme
    Related to-do: Bergen: Duplicate the KS Læring theme and change logo, footer and light adjustmenst to colors according to the Bergen kommune web profile.
    https://basecamp.com/2124401/projects/5622897/todos/215224321

---
+++ theme/bergen/layout/inc/footer.php
@@ -0,0 +1,17 @@
+<footer id="page-footer">
+    <div id="page-footer-inner" class="wrapper clearfix row-fluid">
+        <div class="footer_mountain_container span12">
+            <div class="mountains"></div>
+        </div>
+    </div>
+</footer>
+<footer id="sub-footer" class="container-fluid">
+    <div class="row-fluid">
+        <p class="text span12">
+            Bergen kommune benytter KS Læring for kursadministrasjon og
+            nettlæring.<br/>
+            Systemeier: HR-avdelingen. For brukerstøtte kontakt Helpdesk eller «Meld
+            feil»
+        </p>
+    </div>
+</footer>
