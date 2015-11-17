c7afcc95eb33df7d77422d9b33ced7600099d210
uhunkler <urs.hunkler@unodo.de>
2015-11-17 09:31:50 +0100

    theme_bergen - Add the bergen theme as a kommit child theme
    Related to-do: Bergen: Duplicate the KS Læring theme and change logo, footer and light adjustmenst to colors according to the Bergen kommune web profile.
    https://basecamp.com/2124401/projects/5622897/todos/215224321

---
+++ theme/bergen/settings.php
@@ -0,0 +1,69 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle's bergen theme, an example of how to make a Bootstrap theme
+ *
+ * DO NOT MODIFY THIS THEME!
+ * COPY IT FIRST, THEN RENAME THE COPY AND MODIFY IT INSTEAD.
+ *
+ * For full information about creating Moodle themes, see:
+ * http://docs.moodle.org/dev/Themes_2.0
+ *
+ * @package   theme_bergen
+ * @copyright 2015 eFaktor
+ * @author    Urs Hunkler {@link urs.hunkler@unodo.de}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+
+    // Invert Navbar to dark background.
+    $name = 'theme_bergen/invert';
+    $title = get_string('invert', 'theme_bergen');
+    $description = get_string('invertdesc', 'theme_bergen');
+    $setting = new admin_setting_configcheckbox($name, $title, $description, 0);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Logo file setting.
+    $name = 'theme_bergen/logo';
+    $title = get_string('logo','theme_bergen');
+    $description = get_string('logodesc', 'theme_bergen');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Custom CSS file.
+    $name = 'theme_bergen/customcss';
+    $title = get_string('customcss', 'theme_bergen');
+    $description = get_string('customcssdesc', 'theme_bergen');
+    $default = '';
+    $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Footnote setting.
+    $name = 'theme_bergen/footnote';
+    $title = get_string('footnote', 'theme_bergen');
+    $description = get_string('footnotedesc', 'theme_bergen');
+    $default = '';
+    $setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+}
