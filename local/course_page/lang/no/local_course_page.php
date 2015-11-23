da27599b3ba39b1bf2516d676eca8f1b94fb1c92
Alf Martin Johnsen <amj@Alfs-MacBook-Pro.local>
2015-11-23 08:19:14 +0100

    Translate

--- local/course_page/lang/no/local_course_page.php
+++ local/course_page/lang/no/local_course_page.php
@@ -10,47 +10,44 @@
  * @author          eFaktor     (fbv)
  */
 
-$string['pluginname']           = 'Hjemmesider';
-$string['course_page:manage']   = 'Administrere hjemmesider';
-
-$string['checkbox_home']            = 'Opprett hjemmeside';
+$string['block_staff']              = 'Ressurspersoner';
+$string['btn_more']                 = 'Mer';
+$string['btn_more']                 = 'Mer';
 $string['checkbox_home_help']       = 'Hvis hjemmeside skal genereres automatisk';
-$string['home_page']                = 'Kursets hjemmeside';
+$string['checkbox_home']            = 'Opprett hjemmeside';
+$string['class_course']             = 'Klasserom';
+$string['course_page:manage']   = 'Administrere hjemmesider';
+$string['current_graphic']          = 'Gjeldende grafikk';
+$string['edit_home_page']           = 'Hjemmeside';
+$string['elearning']                 = 'eLearning';
+$string['home_about']               = 'Om dette kurset';
+$string['home_coordinater']         = 'Koordinator';
+$string['home_course_id']           = 'ID';
+$string['home_current_video']       = 'Gjeldende video';
+$string['home_delete_video']        = 'Slett eksisterende hjemmesidevideo';
 $string['home_desc']                = 'Om dette kurset';
-
 $string['home_graphics']            = 'Hjemmesidegrafikk';
 $string['home_graphicstitle']       = 'Hjemmesidegrafikk title';
-$string['current_graphic']          = 'Gjeldende grafikk';
-$string['home_video']               = 'Hjemmesidevideo';
-$string['home_delete_video']        = 'Slett eksisterende hjemmesidevideo';
-$string['home_current_video']       = 'Gjeldende video';
-
-$string['sel_course_manager']       = 'Velg en ....';
-$string['edit_home_page']           = 'Hjemmeside';
-$string['home_about']               = 'Om dette kurset';
+$string['home_page']                = 'Kursets hjemmeside';
 $string['home_published']           = 'Sist oppdatert';
-$string['home_coordinater']         = 'Koordinator';
-$string['home_teachers']            = 'Instruktører';
 $string['home_ratings']             = 'Kursvurderinger';
-$string['ratings_avg']              = 'Gjennomsnitt';
+$string['home_register']            = 'Meld deg på';
+$string['home_start']               = 'Start';
+$string['home_teachers']            = 'Instruktører';
+$string['home_type']                = 'Kurstype';
+$string['home_video']               = 'Hjemmesidevideo';
+$string['home_visible']             = 'Publiseringsstatus';
+$string['net_course']               = 'Nettkurs';
+$string['not_comments']             = 'Ingen kommentarer';
+$string['pluginname']           = 'Hjemmesider';
 $string['rate_avg']                 = 'Middels';
-$string['rate_users']               = 'Deltakervurderinger';
+$string['rate_bad']                 = 'Dårlig';
 $string['rate_exc']                 = 'Kjempebra';
 $string['rate_good']                = 'Bra';
 $string['rate_poor']                = 'Ikke så bra';
-$string['rate_bad']                 = 'Dårlig';
-$string['title_reviews']            = 'Revisjon';
-$string['home_register']            = 'Meld deg på';
-$string['home_start']               = 'Start';
-$string['home_visible']             = 'Publiseringsstatus';
-$string['home_course_id']           = 'ID';
-$string['block_staff']              = 'Kurslærere';
-$string['home_type']                = 'Kurstype';
-$string['btn_more']                 = 'Les mer';
-$string['net_course']               = 'Nettkurs';
-$string['elearning']                 = 'eLearning';
-$string['class_course']             = 'Klasserom';
-$string['whitepaper']               = 'Dokumentformat';
+$string['rate_users']               = 'Deltakervurderinger';
+$string['ratings_avg']              = 'Gjennomsnitt';
+$string['sel_course_manager']       = 'Velg en ....';
 $string['single']                   = 'Single Activity';
-$string['btn_more']                 = 'Mer';
-$string['not_comments']             = 'Ingen kommentarer';
+$string['title_reviews']            = 'Revisjon';
+$string['whitepaper']               = 'Dokumentformat';
