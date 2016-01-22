fb2f0826b36a61d08930fe780753ca839ea3e31b
Alf Martin Johnsen <amj@Alfs-MacBook-Pro.local>
2016-01-22 08:47:21 +0000

    Translated

--- blocks/rate_course/lang/no/block_rate_course.php
+++ blocks/rate_course/lang/no/block_rate_course.php
@@ -29,8 +29,8 @@
 
 $string['rate_course:addinstance'] = 'Legg til en ny kursvurderingsblokk';
 $string['rate_course:myaddinstance'] = 'Rettighet til å vurdere kurs';
-$string['pluginname'] = 'Deltakervurderinger';
-$string['courserating'] = 'Deltakervurderinger';
+$string['pluginname'] = 'Kursvurderinger';
+$string['courserating'] = 'Kursvurderinger';
 
 $string['completed'] = 'Tusen takk. Du har allerede vurdert dette kurset.';
 $string['giverating'] = 'Gi en vurdering';
@@ -38,8 +38,8 @@ $string['intro'] = 'Vennligst angi hvor høyt du vurderer dette kurset.';
 $string['submit'] = 'Send inn min vurdering';
 $string['noguestuseage'] = 'For å vurderdere dette kurset må du være innlogget.';
 $string['rate_course:rate'] ='Gi en vurdering til et kurs';
-$string['rating_alt0'] ='Ingen vurderinger gitt';
-$string['rating_altnum'] ='Bedømt til: {$a} stjerner.';
+$string['rating_alt0'] ='Course rating: Ingen vurderinger gitt.';
+$string['rating_altnum'] ='Course rating: {$a} stjerner.';
 $string['rating_users'] ='Vurdert av {$a} kursdeltakere)';
 $string['editingsitehome'] = 'Denne blokken vil kun vise kursvurderinger på kurssiden, og skjules alle andre steder.';
 
@@ -47,7 +47,7 @@ $string['editingsitehome'] = 'Denne blokken vil kun vise kursvurderinger på kur
 $string['survey'] = 'Hva er spørreskjemaets navn?';
 $string['survey_help'] = 'Dette er spørreundersøkelsen som det blir lenket til (la feltet være tomt for IKKE å lenke til spørreundersøkelsen)';
 $string['viewreview'] = 'Vis responser fra spørreundersøkelsen';
-$string['add_comments'] = 'Skriv inn din kommentar';
+$string['add_comments'] = 'Legg til kommentar';
 
-$string['minum_rates']          = 'Minimum rates';
-$string['minum_rates_help']     = 'Minimum number of rates to publish the results';
+$string['minum_rates']          = 'Minimum antall vurderinger';
+$string['minum_rates_help']     = 'Minimum antall vurderinger før resultatet publiseres';
