bdde2f99a7e121d11cf07b16262291ba4f51729d
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-03-07 14:34:28 +0100

    Municipality block and user profile

---
+++ user/profile/field/municipality/version.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Extra Profile Field Muncipality - Version
+ *
+ * Description
+ *
+ * @package         user/profile
+ * @subpackage      field/municipality
+ * @copyright       2014        eFaktor {@link http://www.efaktor.no}
+ *
+ * @creationDate    19/11/2014
+ * @author          eFaktor     (fbv)
+ *
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2016030700;                    // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2014050800;                    // Requires this Moodle version
+$plugin->component = 'profilefield_municipality';   // Full name of the plugin (used for diagnostics)
+
