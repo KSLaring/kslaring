3fd5e3c9b3890726fab3d137feaeea30b9c0dbfa
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-03-08 09:38:34 +0100

    Bergen. Kommit && Frikomport themes

---
+++ theme/frikomport/layout/inc/footer.php
@@ -0,0 +1,18 @@
+<footer id="page-footer">
+    <div id="page-footer-inner" class="wrapper clearfix">
+        <div class="footer-left">
+            <div class="footer-logo">
+            </div>
+            <p>© 2014 KOMMUNESEKTORENS ORGANISASJON</p>
+            <?php echo $OUTPUT->return_to_role(); ?>
+        </div>
+        <div class="footer-right">
+            <div class="social">
+                <div class="col1"><a href="http://facebook.com/ksfrikomport" target=_blank" alt="facebook icon"><i class="fa fa-facebook
+                fa-2x" id="icon_facebook" aria-hidden="true"></i></a></div>
+                <div class="col2"><a href="https://twitter.com/KSKommIT" target=_blank" alt="twitter icon"><i class="fa fa-twitter
+                fa-2x" id="icon_twitter" aria-hidden="true"></i></a></div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ theme/kommit/renderers.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once('renderers/core_renderer.php');
+require_once('renderers/course_renderer.php');
