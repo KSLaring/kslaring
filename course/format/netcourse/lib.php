db3cbad0811c8f83277fd5e125e7dd35298c7b0e
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-03-21 16:09:06 +0100

    Course Lations && Sectors. Course format & Course Home Page

--- course/format/netcourse/lib.php
+++ course/format/netcourse/lib.php
@@ -760,6 +760,7 @@ class format_netcourse extends format_base {
                 case 'homesummary':
                 case 'pagegraphics':
                 case 'pagevideo':
+                case 'manager':
                     course_page::addCourseHomePage_Section($mform, $optionname);
 
                     break;
