ebb78ec761441b9c829164766d51155d515f7040
uhunkler <urs.hunkler@unodo.de>
2016-04-01 12:30:32 +0200

    theme_bergen - update the Moodle theme verison information

--- theme/bergen/version.php
+++ theme/bergen/version.php
@@ -31,9 +31,12 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016040100;
-$plugin->requires  = 2015111602;
+$plugin->version = 2016040101;
+$plugin->release = '3.0.0';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->requires = 2015111602;
 $plugin->component = 'theme_bergen';
 $plugin->dependencies = array(
-    'theme_bootstrapbase'  => 2015111600,
+    'theme_bootstrapbase' => 2015111600,
+    'theme_kommit' => 2016040100,
 );
