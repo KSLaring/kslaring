880d8ee7d41f937ce64cef15d327f83c683220a3
uhunkler <urs.hunkler@unodo.de>
2016-04-08 10:26:25 +0200

    ksl themes - correct http to https requests in the pages
    Related to-do: KSL: Javascript error in the footer in the 2.7-theme
    https://3.basecamp.com/3304540/buckets/676070/todos/102248974

--- theme/bergen/layout/inc/header.php
+++ theme/bergen/layout/inc/header.php
@@ -69,13 +69,13 @@ if ($loggedin) {
     ed24ChatObj = {
         'entrypoint': 'Kommuneforlaget_Main',
         'mainid': 'A53',
-        'Online': '<img src="http://www.kommuneforlaget.no/filestore/dev/GFX/nettpratonline.png"/>',
-        'Offline': '<img src="http://www.kommuneforlaget.no/filestore/dev/GFX/nettpratoffline.png"/>',
+        'Online': '<img src="https://www.kommuneforlaget.no/filestore/dev/GFX/nettpratonline.png"/>',
+        'Offline': '<img src="https://www.kommuneforlaget.no/filestore/dev/GFX/nettpratoffline.png"/>',
         'Busy': '<img src="https://login.edialog24.com/images/standard2-offline-no.jpg"/>',
         'webserverAddress': 'https://login.edialog24.com',
         'urlToOpen': 'https://login.edialog24.com/chattemplate/Kommuneforlaget_Main/index.html',
         'windowSettings': 'width=600,height=800,status=0,scrollbars=0,titlebar=0'
     };
 </script>
-<script src="http://login.edialog24.com/ChatClient3/EntrypointScript4.js"
+<script src="https://login.edialog24.com/ChatClient3/EntrypointScript4.js"
         type="text/javascript"></script>
