f31806347a18b2bd8d064e73064f3614ddb15178
uhunkler <urs.hunkler@unodo.de>
2016-04-22 10:08:16 +0200

    local_course_page - location.js - merge the 2.7 changes

--- local/course_page/yui/location/location.js
+++ local/course_page/yui/location/location.js
@@ -10,30 +10,55 @@
  */
 
 YUI.add('moodle-local_course_page-location', function(Y) {
-
     M.local_course_page = M.local_course_page || {};
-    M.local_course_page.location = function(params) {
-        var self = this;
-        Y.delegate('click', function(e){
-            var panel = new M.core.dialogue({
-                id: 'location',
-                headerContent: params['header'],
-                bodyContent:params['content'],
-                draggable: true,
-                visible: true,
-                center: false,
-                modal: true,
-                render:true,
-                width: '70%'
-            });
+    M.local_course_page.location = function (params) {
+        var self = this,
+            winHeight = Y.one("body").get("winHeight"),
+            panelWidth = (winHeight < 900) ? '90%' : '70%';
 
-            Y.one('#location').setStyle('left','25px');
-            Y.one('#location').setStyle('top','50px');
-            Y.one('#location').setStyle('bottom','0px');
-            Y.one('#location').setStyle('width','70%');
-            Y.one('#location').setStyle('position','fixed');
+        // Define the responsive dialogue, render the dialogue but don't show it.
+        var panel = new M.core.dialogue({
+            id: 'location',
+            headerContent: params['header'],
+            bodyContent: params['content'],
+            draggable: true,
+            visible: false,
+            center: true,
+            modal: true,
+            render: true,
+            responsive: true,
+            width: panelWidth
+        });
 
+        // Set the click event handler to show the panel on click.
+        Y.delegate('click', function (e) {
             panel.show();
-        },Y.one(document.body), '#show_location');
+        }, Y.one(document.body), '#show_location');
+
+        // On window-resize change the width
+        // and the center property depending on screen height.
+        Y.use('event-resize', function(Y) {
+            Y.on('windowresize', function () {
+                var panelCenter = null;
+                winHeight = Y.one("body").get("winHeight");
+
+                if (winHeight < 900) {
+                    panelWidth = '90%';
+                    panelCenter = false;
+
+                    var bb = panel.get('boundingBox');
+                    bb.setStyles({'left' : '5%',
+                      'top' : '10px'});
+                } else {
+                    panelWidth = '70%';
+                    panelCenter = true;
+                }
+
+                panel.set('width', panelWidth);
+                panel.set('center', panelCenter);
+
+                // console.log(panel.get('width'));
+            }, this);
+        });
     };
 });
