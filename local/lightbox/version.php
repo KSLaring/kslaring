897610538dfcba9be24d880052b4f82142e86a26
uhunkler <urs.hunkler@unodo.de>
2016-04-28 11:08:55 +0200

    local_lighbox - correct lightbox opening - workaround origin issues with https links to external sources
    Related to-do: KSL: URL lightbox opens in standard popup on http, and empty lightbox content on https. When closing the popup, the lightbox effect blocks the window.
    https://3.basecamp.com/3304540/buckets/676070/todos/113128423

--- local/lightbox/version.php
+++ local/lightbox/version.php
@@ -11,9 +11,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version    = 2016030700; /* The current plugin version (Date: YYYYMMDDXX)  */
-$plugin->requires   = 2012061700; /* Requires this Moodle version                   */
-$plugin->component  = 'local_lightbox'; /* Full name of the plugin (used for diagnostics) */
-
-/* Dependencies */
-$plugin->dependencies = array('local_scorm_lightbox' => 2016030700);
+$plugin->version = 2016042800; /* The current plugin version (Date: YYYYMMDDXX)  */
+$plugin->requires = 2012061700; /* Requires this Moodle version                   */
+$plugin->component = 'local_lightbox'; /* Full name of the plugin (used for diagnostics) */
