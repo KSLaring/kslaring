47df34e4d529e624a8599f7a9ce14c07d2658c48
uhunkler <urs.hunkler@unodo.de>
2016-04-28 14:14:41 +0200

    theme_stavanger - added
    Related to-do: SVG: Create the new Stavanger theme for KS Læring 3.0 based on the approved sketch
    https://basecamp.com/2124401/projects/5094077/todos/243420410#comment_411289774

---
+++ theme/stavanger/lang/en/theme_stavanger.php
@@ -0,0 +1,98 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_stavanger', language 'en'
+ *
+ * @package   theme_stavanger
+ * @copyright 2016 eFaktor
+ * @author    Urs Hunkler {@link urs.hunkler@unodo.de}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = 'Hey, I\'m stavanger';
+
+$string['configtitle'] = 'Stavanger';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
+
+$string['invert'] = 'Invert navbar';
+$string['invertdesc'] = 'Swaps text and background for the navbar at the top of the page between black and white.';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Please upload your custom logo here if you want to add it to the header.<br>
+If the height of your logo is more than 75px add the following CSS rule to the Custom CSS box below.<br>
+a.logo {height: 100px;} or whatever height in pixels the logo is.';
+
+$string['pluginname'] = 'Stavanger';
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+
+
+// additional block regions
+$string['region-content-top'] = 'Content top';
+$string['region-content-bottom'] = 'Content bottom';
+$string['region-top'] = 'Top';
+$string['region-bottom'] = 'Bottom';
+$string['region-hidden-dock'] = 'Admin Only';
+
+/* My Dashboard custommenu dropdown */
+$string['mydashboard'] = 'My Dashboard';
+
+/* Edit mode toggle */
+$string['editoff'] = 'off';
+$string['editon'] = 'on';
+$string['edit_label'] = 'Edit mode:';
+
+/* Set my home page */
+$string['sethome'] = 'Set as my home page';
+$string['resethome'] = 'Reset to default page';
+
+/* Read more */
+$string['readmore'] = 'Read more';
+
+/* JS toggle admin login */
+$string['adminlogin'] = 'admin login';
+
+/* Hidden blocks area */
+$string['visibleadminonly'] = 'Blocks moved into the area below will only be seen by teachers and admins.';
+$string['adminmenuentry'] = 'Bottom blocks';
+
+/* Capabilities */
+$string['stavanger:viewhiddenblocks'] = 'View hidden blocks';
+$string['stavanger:frontpageviewhiddenblocks'] = 'View hidden frontpage blocks';
+
+/* Edit frontpage image */
+$string['editimage'] = 'Edit image';
+$string['editimage_alt'] = 'Edit image alt';
+
+/* Course category page */
+$string['expandall_initial'] = 'Collapse/expand all (Expand a section first)';
+
+/* Set my home page */
+$string['sethome']      = 'Set as my home page';
+$string['resethome']    = 'Reset to default page';
+
+/* Footer text */
+$string['footertext']    = 'Stavanger kommune benytter KS Læring for kursadministrasjon og e-læring.<br/>
+Systemeier: Personal og organisasjon ved HR-utvikling.';
+$string['footerhelpurl']    = 'http://www.performit.no/performitplayer/html/v-2-0/bk-laering/bk-laering.how2';
+$string['footerbrukerhelp']    = 'Brukerveiledning';
