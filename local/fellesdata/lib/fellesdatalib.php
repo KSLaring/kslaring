9a6225b17c2e2faea677d7f0cc83e260972826ba
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-04-29 10:16:02 +0200

    Fellesta testing

--- local/fellesdata/lib/fellesdatalib.php
+++ local/fellesdata/lib/fellesdatalib.php
@@ -1552,60 +1552,64 @@ class FS {
                 /* Import content file into temporary tables    */
                 while(!feof($responseFile)) {
                     $lineFile = fgets($responseFile);
-                    $lineFile = json_decode($lineFile);
+
+                    if ($lineFile) {
+                        echo "Line : " . $lineFile . "</br>";
+                    }
+                    //$lineFile = json_decode($lineFile);
 
                     /* Get New Entry    */
-                    $newEntry = $lineFile->newRecord;
+                    //$newEntry = $lineFile->newRecord;
 
                     /* Get Action       */
-                    switch (trim($lineFile->changeType)) {
-                        case ADD_ACTION:
-                            $action = 0;
+                    //switch (trim($lineFile->changeType)) {
+                    //    case ADD_ACTION:
+                    //        $action = 0;
 
-                            break;
-                        case UPDATE_ACTION:
-                            $action = 1;
+                    //        break;
+                    //    case UPDATE_ACTION:
+                    //        $action = 1;
 
-                            break;
-                        case DELETE_ACTION:
-                            /* Old Entry        */
-                            if (isset($lineFile->oldRecord)) {
-                                $newEntry = $lineFile->odlRecord;
-                            }//if_old_record
+                    //        break;
+                    //    case DELETE_ACTION:
+                    //        /* Old Entry        */
+                    //        if (isset($lineFile->oldRecord)) {
+                    //            $newEntry = $lineFile->odlRecord;
+                    //        }//if_old_record
 
-                            $action = 2;
+                    //        $action = 2;
 
-                            break;
-                    }//action
+                    //        break;
+                    //}//action
 
                     /* Import in the right table   */
-                    switch ($type) {
-                        case IMP_USERS:
+                    //switch ($type) {
+                    //    case IMP_USERS:
                             /* FS Users     */
                             //self::ImportTemporary_FSUsers($action,$newEntry);
 
-                            break;
-                        case IMP_COMPANIES:
+                    //        break;
+                    //    case IMP_COMPANIES:
                             /* FS Companies */
-                            self::ImportTemporary_FSCompany($action,$newEntry);
+                            //self::ImportTemporary_FSCompany($action,$newEntry);
 
-                            break;
-                        case IMP_JOBROLES:
+                    //        break;
+                    //    case IMP_JOBROLES:
                             /* FS JOB ROLES */
                             //self::ImportTemporary_FSJobRoles($action,$newEntry);
 
-                            break;
-                        case IMP_COMPETENCE_COMP:
+                    //        break;
+                    //    case IMP_COMPETENCE_COMP:
                             /* Competence Company */
                             //self::ImportTemporary_CompetenceCompany($action,$newEntry);
 
-                            break;
-                        case IMP_COMPETENCE_JR:
+                    //        break;
+                    //    case IMP_COMPETENCE_JR:
                             /* Competence Job Role  */
                             //self::ImportTemporary_CompetenceJobRole($action,$newEntry);
 
-                            break;
-                    }//type
+                    //        break;
+                    //}//type
 
                 }//file
 
