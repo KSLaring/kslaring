9af584a15d2242d43a0912aa78c5abec9e8c2700
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-05-09 12:28:25 +0200

    Fellesdata Testing. Synchronization Users

--- local/fellesdata/cron/fellesdatacron.php
+++ local/fellesdata/cron/fellesdatacron.php
@@ -620,7 +620,7 @@ class FELLESDATA_CRON {
 
         try {
             /* Get user to synchronize  */
-            $rdo = $DB->get_records('fs_imp_users',array('imported' => '0'),'','*',0,1000);
+            $rdo = $DB->get_records('fs_imp_users',array('imported' => '0'),'','*',0,4000);
 
             /* Prepare data */
             if ($rdo) {
