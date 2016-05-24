f412f3b8747c31579483bad987bd204a859dc31b
Alf Martin Johnsen <amj@efaktor.no>
2016-05-24 12:12:55 +0200

    Added Last updated

--- local/course_page/renderer.php
+++ local/course_page/renderer.php
@@ -303,8 +303,8 @@ class local_course_page_renderer extends plugin_renderer_base {
              * Description
              * Change Start Date for Modified Date
              */
-            //$out .= '<h5 class="title_home chp-title">' . get_string('home_published','local_course_page') . '</h5>';
-            //$out.= '<div class="extra_home chp-content">' . userdate($course->timemodified,'%d.%m.%Y', 99, false) . '</div>';
+            $out .= '<h5 class="title_home chp-title">' . get_string('home_published','local_course_page') . '</h5>';
+            $out.= '<div class="extra_home chp-content">' . userdate($course->timemodified,'%d.%m.%Y', 99, false) . '</div>';
 
             $str_format = 'format_' . $course->format;
             switch ($course->format) {
