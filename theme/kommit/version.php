b106f2cec335ac2ec3fd0009eb72f3b1e0a8f4ec
uhunkler <urs.hunkler@unodo.de>
2016-06-09 09:47:09 +0200

    theme_kommit - Remove the "Read more" button on the course summary
    KSL: Remove the "Read more" button on the course summary
    https://3.basecamp.com/3304540/buckets/676070/todos/138634708

--- theme/kommit/version.php
+++ theme/kommit/version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016031600;
+$plugin->version   = 2016060900;
 $plugin->requires  = 2013110500;
 $plugin->component = 'theme_kommit';
 $plugin->dependencies = array(
