f51c2c59f7912a7b23197a7afca941f55cb10b68
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-06-15 12:46:52 +0200

    Report Manager. Job roles && Super users

--- report/manager/job_role/edit_job_role.php
+++ report/manager/job_role/edit_job_role.php
@@ -52,18 +52,25 @@ $PAGE->set_url($url);
 $PAGE->set_context($site_context);
 $PAGE->set_title($SITE->fullname);
 $PAGE->set_heading($SITE->fullname);
+$PAGE->navbar->add(get_string('job_roles', 'report_manager'),$return_url);
+$PAGE->navbar->add(get_string('edit_job_roles', 'report_manager'));
 
 /* Info Super Users */
 $superUser  = CompetenceManager::IsSuperUser($USER->id);
 $myAccess   = CompetenceManager::Get_MyAccess($USER->id);
 
+/* Check permissions */
 if (!$superUser) {
     require_capability('report/manager:edit', $site_context);
     $PAGE->navbar->add(get_string('report_manager','local_tracker_manager'),$return);
+}else {
+    /* Check job role is valid  */
+    if (!job_role::CheckJobRoleAccess($myAccess,$job_role_id)){
+        print_error('nopermissions', 'error', '', 'report/manager:edit');
+    }
 }//if_SuperUser
-$PAGE->navbar->add(get_string('job_roles', 'report_manager'),$return_url);
-$PAGE->navbar->add(get_string('edit_job_roles', 'report_manager'));
 
+/* Security */
 if (empty($CFG->loginhttps)) {
     $secure_www_root = $CFG->wwwroot;
 } else {
