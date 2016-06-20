89be3d73dabb9d661bd57810eb5d8b5a09c6f30d
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-06-20 18:05:39 +0200

    Felelsdata

--- local/fellesdata/lib/fellesdatalib.php
+++ local/fellesdata/lib/fellesdatalib.php
@@ -1025,9 +1025,11 @@ class FSKS_USERS {
                             ks.hierarchylevel,
                             fs.prioritet,
                             fs.action
-                     FROM	  {fs_imp_managers_reporters}  fs
-                        JOIN  {ksfs_company}		  fsk	ON  fsk.fscompany = fs.org_enhet_id
-                        JOIN  {ks_company}			  ks	ON	ks.companyid  = fsk.kscompany
+                     FROM	  {fs_imp_managers_reporters}   fs
+                        JOIN  {user}                        u         ON      u.username = fs.fodselsnr
+                                                                      AND     u.deleted  = 0
+                        JOIN  {ksfs_company}		        fsk	ON  fsk.fscompany = fs.org_enhet_id
+                        JOIN  {ks_company}			        ks	ON	ks.companyid  = fsk.kscompany
                      WHERE	fs.imported	= :imported ";
 
             /* Check if it's a manual execution */
@@ -1431,6 +1433,8 @@ class FSKS_USERS {
                                 GROUP_CONCAT(DISTINCT fs.stillingskode ORDER BY fs.stillingskode SEPARATOR ',') as 'fsjobroles',
                                 GROUP_CONCAT(DISTINCT fs.id ORDER BY fs.id SEPARATOR ',') as 'impkeys'
                      FROM	    {fs_imp_users_jr}	  fs
+                        JOIN    {user}                u         ON      u.username = fs.fodselsnr
+                                                                AND     u.deleted  = 0
                         -- COMPANY
                         JOIN	{ksfs_company}		  ksfs 		ON 		ksfs.fscompany 		= fs.ORG_ENHET_ID
                         JOIN	{ks_company}		  ks	    ON		ks.companyid		= ksfs.kscompany
