9f2c4025d6a8785cb084f28ac45dcfdf6c41dcca
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-06-20 18:30:17 +0200

    Felelsdata

--- local/fellesdata/lib/fellesdatalib.php
+++ local/fellesdata/lib/fellesdatalib.php
@@ -1034,7 +1034,7 @@ class FSKS_USERS {
 
             /* Check if it's a manual execution */
             if ($SESSION->manual) {
-                $sql .= " LIMIT 0,1 ";
+                $sql .= " LIMIT 0,10 ";
             }//if_manual
 
             /* Execute  */
