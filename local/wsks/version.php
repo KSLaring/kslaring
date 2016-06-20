1f1d465a48f643717105afbdc53496d1304a8e8a
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-06-20 16:50:01 +0200

    Version Fellesdata Web Service

--- local/wsks/version.php
+++ local/wsks/version.php
@@ -11,5 +11,5 @@
  */
 
 //$plugin->version    = '2015103000';         /* The plugin current version (DATE: YYYYMMDDXX) */
-$plugin->version    =   '2016061404';
+$plugin->version    =   '2016062000';
 $plugin->component  =   'local_wsks';
