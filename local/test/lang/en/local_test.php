7874490ad051d33c24be6adf3d511c224018f7f4
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-06-24 10:08:07 +0200

    Fellesdata Cron

---
+++ local/test/lang/en/local_test.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Fellesdata Integration - Language Settings (English)
+ *
+ * @package         local/fellesdata
+ * @subpackage      lang
+ * @copyright       2014        eFaktor {@link http://www.efaktor.no}
+ *
+ * @creationDate    01/02/2016
+ * @author          eFaktor     (fbv)
+ *
+ */
+
+$string['pluginname']           = 'Local Test';
+
+$string['test:manage']    = 'Local Test';
+
+$string['cron_activate']            = 'Enabled';
+$string['cron_deactivate']          = 'Disabled';
+
+
+
