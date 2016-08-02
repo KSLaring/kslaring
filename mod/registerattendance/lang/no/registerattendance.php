6a155ee00d95a5c31837a04b9add09e9f46ea8bd
Alf Martin Johnsen <amj@efaktor.no>
2016-08-02 13:27:10 +0200

    Replaced the NO lang file with the correct one and translated

--- mod/registerattendance/lang/no/registerattendance.php
+++ mod/registerattendance/lang/no/registerattendance.php
@@ -25,23 +25,52 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['registerattendance:addinstance'] = 'Legg til ny fullføringsrapport';
-$string['registerattendancetext'] = 'Fullføringsrapport tekst';
-$string['modulename'] = 'Fullføringsrapport';
-$string['modulename_help'] = 'Fullføringsrapportmodulen viser progresjonen for påmeldte brukere.';
+$string['registerattendance:addinstance'] = 'Legg til ny frammøteregistering';
+$string['registerattendancetext'] = 'Frammøteregistrering tekst';
+$string['modulename'] = 'Registrere frammøte';
+$string['modulename_help'] = 'Modulen for frammøteregistrering viser hvem som har møtt på kurset.';
 $string['modulename_link'] = 'mod/registerattendance/view';
-$string['modulenameplural'] = 'Fullføringsrapporter';
-$string['pluginadministration'] = 'Administrasjon av fullføringsrapport';
-$string['pluginname'] = 'Fullføringsrapport';
+$string['modulenameplural'] = 'Registrere frammøte';
+$string['pluginadministration'] = 'Administrasjon av frammøteregistrering';
+$string['pluginname'] = 'Registrere frammøte';
 
-$string['completionprogressdetails'] = 'Detaljert visning av fullføringsrapport';
-$string['completionstatus:addinstance'] = 'Legg til en ny ressurs for kursfullføring';
-$string['criteriagroup'] = 'Fullføringsbetingelser';
+$string['completionprogressdetails'] = 'Aktivitetsfullføringsdetaljer';
+$string['completionstatus:addinstance'] = 'Legg til en ny blokk for fullførtregistrering';
+$string['criteriagroup'] = 'Kriteriegruppe';
 $string['firstofsecond'] = '{$a->first} av {$a->second}';
-$string['pluginname'] = 'Register attendance';
-$string['requirement'] = 'Krav';
+$string['pluginname'] = 'Registrere frammøte';
+$string['requirement'] = 'Forutsetninger';
 $string['returntocourse'] = 'Gå tilbake til kurset';
 
-$string['err_nocriteria'] = 'Ingen kriterier gitt';
-$string['notenrolled'] = 'Ingenting å vise fordi du ikke er påmeldt dette kurset.';
-$string['notcompletionenabled'] = 'Ingenting å vise fordi kursfullføring ikke er aktivert i dette kurset.';
+$string['err_nocriteria'] = 'Ingen kriterier';
+$string['notenrolled'] = 'Ingenting å vise siden du ikke er påmeldt i dette kurset.';
+$string['notcompletionenabled'] = 'Ingenting å vise siden aktivitetsfullføring ikke er aktivert for kurset.';
+
+$string['cachedef_registerattendance'] = 'Cache for frammøteregistrering';
+$string['completionattended'] = 'Kursdeltakere må være registrert som møtt for å kunne fullføre denne aktiviteten.';
+$string['youhaveattended'] = 'Du har {$a} i kurset.';
+
+$string['view_title'] = 'Registrere frammøte';
+$string['view_id'] = 'BrukerID';
+$string['view_firstname'] = 'Fornavn';
+$string['view_lastname'] = 'Etternavn';
+$string['view_fullname'] = 'Fullt navn';
+$string['view_municipality'] = 'Kommune';
+$string['view_workplace'] = 'Arbeidssted';
+$string['view_attended'] = 'Møtt';
+$string['list'] = 'vis';
+$string['showattended_all'] = 'Vis alle';
+$string['showattended_attended'] = 'Vis frammøtte';
+$string['showattended_notattended'] = 'Vis ikke møtt';
+$string['addfilter'] = 'Legg til filter';
+$string['searchnameshort'] = 'Søk ...';
+$string['searchnamelabel'] = 'Søk i brukerens fulle navn';
+$string['showperpage'] = 'Vis brukere pr. side';
+
+$string['bulkregister_title'] = 'Masseregistrering av frammøte';
+$string['bulkregisterbtn'] = 'Masseregistrering';
+$string['haveattended'] = '<span class="attended">{$a->haveattended}</span>/<span 
+class="enrolled">{$a->enrolled}</span> kursdeltakere har møtt.';
+$string['returntoregisterattendance'] = 'Gå tilbake til frammøteregistreringen';
+$string['registerlisted'] = 'Registrer alle viste brukere som møtt';
+$string['unregisterlisted'] = 'Registrer alle viste brukere som ikke møtt';
