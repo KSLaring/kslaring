5929b6807ac0137e8b333c4ff96c9668cbd9ecd1
uhunkler <urs.hunkler@unodo.de>
2016-09-02 17:26:44 +0200

    theme_stavanger Moodle 3.1 update

---
+++ theme/stavanger/db/access.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme stavanger capability definition
+ *
+ * @package   theme_stavanger
+ * @copyright 2016 eFaktor
+ * @author    Urs Hunkler {@link urs.hunkler@unodo.de}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'theme/stavanger:frontpageviewhiddenblocks' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'guest' => CAP_PREVENT,
+            'student' => CAP_PREVENT,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        )
+    ),
+    'theme/stavanger:viewhiddenblocks' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'guest' => CAP_PREVENT,
+            'student' => CAP_PREVENT,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        )
+    ),
+);
---
+++ theme/stavanger/layout/inc/footer.php
@@ -0,0 +1,17 @@
+<footer id="page-footer">
+    <div id="page-footer-inner" class="wrapper clearfix row-fluid">
+        <div class="footer-image-container span12">
+            <div class="footer-image"></div>
+        </div>
+    </div>
+</footer>
+<footer id="sub-footer" class="container-fluid">
+    <div class="row-fluid">
+        <div class="text span10">
+            <?php echo $html->footertext ?>
+        </div>
+        <div class="help-link span2"></div>
+
+        <?php echo $OUTPUT->return_to_role(); ?>
+    </div>
+</footer>
