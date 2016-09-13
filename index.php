ec7a00bc5faf8cd28f684ff4defaf2cc81e2137c
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-09-13 09:28:53 +0200

    Manuallt

--- index.php
+++ index.php
@@ -31,8 +31,7 @@
     require_once('config.php');
     require_once($CFG->dirroot .'/course/lib.php');
     require_once($CFG->libdir .'/filelib.php');
-    
-    echo "From Date: " . $fromDate . "</br>";
+
     redirect_if_major_upgrade_required();
 
     $urlparams = array();
