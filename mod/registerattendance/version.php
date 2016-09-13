ddd7b45301a23c266b4a51ce5742dad51ae84781
uhunkler <urs.hunkler@unodo.de>
2016-09-13 13:07:03 +0200

    mod_ registerattendance - add lang string for »Can register« capability
    Related to-do: KSL: Debug error on the registerattendance
    https://3.basecamp.com/3304540/buckets/676070/todos/220047341

--- mod/registerattendance/version.php
+++ mod/registerattendance/version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016080900; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2013110500; // Requires this Moodle version
-$plugin->component = 'mod_registerattendance'; // Full name of the plugin (used for diagnostics)
+$plugin->version = 2016091300; // The current module version (Date: YYYYMMDDXX).
+$plugin->requires = 2013110500; // Requires this Moodle version.
+$plugin->component = 'mod_registerattendance'; // Full name of the plugin (used for diagnostics).
 $plugin->cron = 0;
