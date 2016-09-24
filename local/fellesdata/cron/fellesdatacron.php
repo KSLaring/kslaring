2f878e149c58fe09f9988c9f229e7093e3053c3e
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-09-24 13:07:15 +0200

    Resource number

--- local/fellesdata/cron/fellesdatacron.php
+++ local/fellesdata/cron/fellesdatacron.php
@@ -805,7 +805,8 @@ class FELLESDATA_CRON {
                 /* Call Web Service */
                 $response = self::ProcessKSService($pluginInfo,KS_SYNC_USER_ACCOUNT,$usersFS);
 
-                echo json_encode($response);
+                echo json_encode($response) . "</br>";
+                echo "ERROR: " . $response['error'] . "</br>";
 
                 //if ($response['error'] == '200') {
                     /* Synchronize Users Accounts FS    */
