89c37d217cae631dbf5fa44350d83fb957af3871
uhunkler <urs.hunkler@unodo.de>
2016-09-28 10:54:04 +0200

    mod_certificate - patch the certificate to add the editor for the customtext
    Related to-do: KSL: Add a text editor to the custom text field in the Certificate plugin.
    https://3.basecamp.com/3304540/buckets/676070/todos/155095558#__recording_240142173

--- mod/certificate/mod_form.php
+++ mod/certificate/mod_form.php
@@ -123,7 +123,22 @@ class mod_certificate_mod_form extends moodleform_mod {
         $mform->setDefault('printteacher', 0);
         $mform->addHelpButton('printteacher', 'printteacher', 'certificate');
 
-        $mform->addElement('textarea', 'customtext', get_string('customtext', 'certificate'), array('cols'=>'40', 'rows'=>'4', 'wrap'=>'virtual'));
+        // Start change uh 2016-09-28.
+        // Add the editor with a reduced button set.
+        //$mform->addElement('textarea', 'customtext', get_string('customtext', 'certificate'), array('cols'=>'40', 'rows'=>'4', 'wrap'=>'virtual'));
+        $options = array();
+        $options['atto:toolbar'] = '
+            collapse = collapse
+            style1 = title, bold, italic
+            list = unorderedlist, orderedlist
+            style2 = underline, strike, subscript, superscript
+            align = align
+            insert = table, clear
+            undo = undo
+            other = html';
+        $mform->addElement('editor', 'customtext', get_string('customtext', 'certificate'),
+            array('cols' => '40', 'rows' => '4', 'wrap' => 'virtual'), $options);
+        // End change uh.
         $mform->setType('customtext', PARAM_RAW);
         $mform->addHelpButton('customtext', 'customtext', 'certificate');
 
@@ -182,4 +197,25 @@ class mod_certificate_mod_form extends moodleform_mod {
 
         return $errors;
     }
+
+    // Start change uh 2016-09-28.
+    /**
+     * Load in existing data as form defaults.
+     *
+     * The editor expects an array for the text with the »format« and the »text«. The certifacte
+     * only stores the text. Change the stored text string into the expected array with the additional format.
+     *
+     * @param mixed $default_values object or array of default values
+     */
+    public function set_data($defaults) {
+        if (!empty($defaults->customtext)) {
+            $defaults->customtext = array(
+                'format' => 1,
+                'text' => $defaults->customtext
+            );
+        }
+
+        parent::set_data($defaults);
+    }
+    // End change uh.
 }
