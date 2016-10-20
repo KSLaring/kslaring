5a8301cef1df2a288f0c71982e7c8784ac8e8567
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-10-20 15:21:04 +0200

    iCalendar

--- enrol/waitinglist/lib.php
+++ enrol/waitinglist/lib.php
@@ -1523,17 +1523,6 @@ class enrol_waitinglist_plugin extends enrol_plugin {
             }
         }//if_Rdo
     }
-
-    private function restore_submethods($odlId,$newId,$courseId) {
-        /* Variables */
-        global $DB;
-        
-        try {
-            
-        }catch (Exception $ex) {
-            throw $ex;
-        }//try_catch
-    }//restore_submethods
     
     /**
      * Restore user enrolment.
@@ -1772,7 +1761,7 @@ class enrol_waitinglist_plugin extends enrol_plugin {
                     $iCal .= "UID:"             . $uid . "\n";
                     $iCal .= "DTSTART:"         . date('Ymd\THis', $course->startdate + 28800) . "\n";
                     if ($location) {
-                        $iCal .= "LOCATION:"        . $location->name . '\n' . $location->address. "\n";
+                        $iCal .= "LOCATION:"        . $location->name . "\n" . $location->address. "\n";
                         if ($location->map) {
                             $iCal .= "URL;VALUE=URI:" . $location->map . "\n";
                         }
@@ -1847,14 +1836,14 @@ class enrol_waitinglist_plugin extends enrol_plugin {
                 $infoLocation->name        = $rdo->name;
                 /* Detail */
                 $infoLocation->detail      = get_string('location_floor','local_friadmin') . ': ' . $rdo->floor;
-                $infoLocation->detail     .= '\n';
+                $infoLocation->detail     .= "\n";
                 $infoLocation->detail     .= get_string('location_room','local_friadmin')  . ': ' . $rdo->room;
-                $infoLocation->detail     .= '\n';
+                $infoLocation->detail     .= "\n";
                 /* Address  */
                 $infoLocation->address     = $rdo->street;
-                $infoLocation->address    .= '\n';
+                $infoLocation->address    .= "\n";
                 $infoLocation->address    .= $rdo->postcode . ' ' . $rdo->city;
-                $infoLocation->address    .= '\n';
+                $infoLocation->address    .= "\n";
                 /* Url Map */
                 $infoLocation->map         = $rdo->urlmap;
             }//if_Rdo
