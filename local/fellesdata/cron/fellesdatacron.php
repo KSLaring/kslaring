c63de9c8ad012795423ba54fb9826e4b529ffef5
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-10-21 17:29:22 +0200

    Felledsata.

--- local/fellesdata/cron/fellesdatacron.php
+++ local/fellesdata/cron/fellesdatacron.php
@@ -785,7 +785,7 @@ class FELLESDATA_CRON {
 
         try {
             /* Get user to synchronize  */
-            $rdo = $DB->get_records('fs_imp_users',array('imported' => '0','action' => 1),'','*',0,400);
+            $rdo = $DB->get_records('fs_imp_users',array('imported' => '0','action' => 1),'','*',0,200);
 
             /* Get Industry Code    */
             if ($pluginInfo->ks_muni) {
