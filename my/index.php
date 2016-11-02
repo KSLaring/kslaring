3fad1ab216a116912f0851b36e4e11ed71d3289a
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-11-02 09:44:55 +0100

    Fake email

--- my/index.php
+++ my/index.php
@@ -188,10 +188,12 @@ if ($PAGE->user_allowed_editing()) {
     //    $button = $OUTPUT->single_button($url, $editstring);
     //    $PAGE->set_button($resetbutton . $button);
     //}
-    require_once('../local/wsks/fellesdata/wsfellesdatalib.php');
-    if (WS_FELLESDATA::IsFakeMail($USER->email)) {
-        $url = new moodle_url('/local/wsks/email/email.php',array('id' => $USER->id));
-        redirect($url);
+    if (!is_siteadmin($USER->id)) {
+        require_once('../local/wsks/fellesdata/wsfellesdatalib.php');
+        if (WS_FELLESDATA::IsFakeMail($USER->email)) {
+            $url = new moodle_url('/local/wsks/email/email.php',array('id' => $USER->id));
+            redirect($url);
+        }
     }
 } else {
     $USER->editing = $edit = 0;
