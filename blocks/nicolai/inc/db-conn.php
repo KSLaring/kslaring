38248c2378e594de1520425112c8426af97bd738
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-11-16 12:51:30 +0100

    Remove block nicolai

--- blocks/nicolai/inc/db-conn.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$conn = mysql_connect('localhost','nikolaidev','Passord123.');
-mysql_select_db('nikolaidev',$conn);
--- blocks/nicolai/lang/en/block_nicolai.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$string['pluginname'] = 'Nicolai block';
-$string['nicolai'] = 'Nicolai';
-$string['nicolai:addinstance'] = 'Add a new Nicolai block';
-$string['nicolai:myaddinstance'] = 'Add a new Nicolai block to the My Moodle Page';
