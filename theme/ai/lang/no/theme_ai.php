c009a49aeac21ad7b2423bd82563c38ecc55c23b
uhunkler <urs.hunkler@unodo.de>
2016-11-29 12:38:00 +0100

    theme_ai - the first commit of the Arbeid og inkludering theme

---
+++ theme/ai/lang/no/theme_ai.php
@@ -0,0 +1,95 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_ai', language 'en'
+ *
+ * @package   theme_ai
+ * @copyright 2016 eFaktor
+ * @author    Urs Hunkler {@link urs.hunkler@unodo.de}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = 'Hei, jeg er Arbeid og inkludering-designet';
+
+$string['configtitle'] = 'Arbeid og inkludering';
+
+$string['customcss'] = 'Egendefinert CSS';
+$string['customcssdesc'] = 'All CSS du legger til her vil benyttes på alle sider og gjør det lettere å gjøre mindre endringer i dette designet.';
+
+$string['footnote'] = 'Bunntekst';
+$string['footnotedesc'] = 'Det du skriver inn her vil vises i bunnteksten på siden på alle sider i portalen.';
+
+$string['invert'] = 'Inverter fargene i navigasjonslinjen';
+$string['invertdesc'] = 'Bytter om fargene på tekst og bakgrunn i navigasjonslinja på toppen av siden.';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Vennligst last opp logoen din her hvis du vil ha en annen i toppteksten.<br>
+Hvis høyden på logoen er mer enn 75px, må du legge til følgende CSS-regel i Egendefinert CSS-feltet under.<br>
+a.logo {height: 100px;} eller hva høyden på logoen i pixler måtte være.';
+
+$string['pluginname'] = 'Arbeid og inkludering';
+
+$string['region-side-post'] = 'Høyre';
+$string['region-side-pre'] = 'Venstre';
+
+
+// additional block regions
+$string['region-content-top'] = 'Innhold-topp';
+$string['region-top'] = 'Topp';
+$string['region-bottom'] = 'Bunn';
+
+/* My Dashboard custommenu dropdown */
+$string['mydashboard'] = 'Mitt dashbord';
+
+/* Edit mode toggle */
+$string['editoff'] = 'Av';
+$string['editon'] = 'På';
+$string['edit_label'] = 'Redigeringsmodus:';
+
+/* Set my home page */
+$string['sethome'] = 'Angi som startside';
+$string['resethome'] = 'Bruk standard startside';
+
+
+/* Read more */
+$string['readmore'] = 'Les mer';
+
+/* JS toggle admin login */
+$string['adminlogin'] = 'admin login';
+
+/* Hidden blocks area */
+$string['visibleadminonly'] = 'Blocks moved into the area below will only be seen by teachers and admins.';
+$string['adminmenuentry'] = 'Bottom blocks';
+
+/* Capabilities */
+$string['ai:viewhiddenblocks'] = 'View hidden blocks';
+$string['ai:frontpageviewhiddenblocks'] = 'View hidden frontpage blocks';
+
+/* Set my home page */
+$string['sethome'] = 'Angi som startside';
+$string['resethome'] = 'Bruk standard startside';
+
+/* Header text */
+$string['laering'] = '';
+$string['system'] = 'Kursadministrasjonssystem for Arbeid og inkludering';
+
+/* Footer text */
+$string['footertext'] = '<strong>Arbeid & Inkludering i NHO Service - Postboks 5473, Majorstuen - 0305 Oslo</strong><br>
+Tlf: 23 08 86 50 - Fax: 23 08 86 59<br>
+© 2001-2016 Arbeid & Inkludering i NHO Service';
+$string['footerhelpurl'] = 'http://www.performit.no/performitplayer/html/v-2-0/bk-laering/bk-laering.how2';
+$string['footerbrukerhelp'] = 'Brukerveiledning';
---
+++ theme/ai/layout/inc/footer.php
@@ -0,0 +1,12 @@
+<footer id="page-footer"></footer>
+<div class="footer-wrapper">
+    <footer id="sub-footer" class="container-fluid">
+        <div class="row-fluid">
+            <div class="text span12">
+                <?php echo $html->footertext ?>
+            </div>
+
+            <?php echo $OUTPUT->return_to_role(); ?>
+        </div>
+    </footer>
+</div>
