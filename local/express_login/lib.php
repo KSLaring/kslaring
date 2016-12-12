892747a12adea6a80568b2b6b1eb3e014470b591
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-12-12 14:00:12 +0100

    Express login

--- local/express_login/lib.php
+++ local/express_login/lib.php
@@ -63,32 +63,7 @@ function local_express_login_myprofile_navigation(core_user\output\myprofile\tre
     return true;
 }//local_express_login_myprofile_navigation
 
-function local_express_login_extend_settings_navigation_old($settingsnav, $context) {
-    /* Plugin Info */
-    $plugin     = get_config('local_express_login');
-    if (($plugin) && (isset($plugin->activate_express))) {
-        if ($plugin->activate_express) {
-            if ($setting_node = $settingsnav->get('usercurrentsettings')) {
-                $str_title = get_string('pluginname', 'local_express_login');
-                $url = new moodle_url('/local/express_login/index.php');
-                /* Create Node  */
-                $express_node = navigation_node::create($str_title,
-                    null,
-                    navigation_node::TYPE_SETTING,'express_login',
-                    'express_login',
-                    null);
-
-                /* Generate PIN CODE    */
-                $express_node->add($str_title,$url);
-                /* Change PIN CODE      */
-                $express_node->add(get_string('btn_change_pin_code','local_express_login'),new moodle_url('/local/express_login/change_express.php'));
-                /* Regenerate Express Link  */
-                $express_node->add(get_string('btn_regenerate_link','local_express_login'),new moodle_url('/local/express_login/regenerate_express.php'));
-
-                $setting_node->add_node($express_node);
-            }//if_usercurrentsettings
-        }
-    }//if_activate
+function local_express_login_extend_settings_navigation($settingsnav, $context) {
 }//local_express_login_extend_settings_navigation
 
 /**
