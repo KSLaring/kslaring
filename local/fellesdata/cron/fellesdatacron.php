a0fc2e360ef0db3b97ef22a4d9257c6b8c012f81
Paqui Bellés Vallejo <fbv@efaktor.no>
2016-12-15 12:16:05 +0100

    Fellesdata

--- local/fellesdata/cron/fellesdatacron.php
+++ local/fellesdata/cron/fellesdatacron.php
@@ -39,7 +39,7 @@ class FELLESDATA_CRON {
     /* PUBLIC */
     /**********/
 
-    public static function cron($fstExecution) {
+    public static function cron_old($fstExecution) {
         /* Variables    */
         global $CFG;
         $pluginInfo = null;
