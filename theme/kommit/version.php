cb1ef73a718642f2cda81c3ea612910b2201bfcc
uhunkler <urs.hunkler@unodo.de>
2016-12-18 12:33:50 +0100

    theme_kommit - Change user nae color to white
    Related to-do: KSL: Missing connection to the custom user menu settings
    https://3.basecamp.com/3304540/buckets/676070/todos/323831294#__recording_324981510

--- theme/kommit/version.php
+++ theme/kommit/version.php
@@ -31,8 +31,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2016121600;
-$plugin->release = '3.1.0 (Build: 2016121400)';
+$plugin->version = 2016121800;
+$plugin->release = '3.1.0 (Build: 2016121800)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2015111602;
 $plugin->component = 'theme_kommit';
