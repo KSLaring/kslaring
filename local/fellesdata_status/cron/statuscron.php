ec3bf7841e7f8d859df53f147f5b7b7956bc9ac8
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-03-01 11:13:07 +0100

    Fellesdata S

--- local/fellesdata_status/cron/statuscron.php
+++ local/fellesdata_status/cron/statuscron.php
@@ -95,43 +95,21 @@ class STATUS_CRON {
             //    for ($i=0;$i<=$total;$i=$i+$limit) {
                     $competence = FSKS_USERS::user_competence_to_synchronize(false,true,$start,$limit);
 
-                    echo $competence . "</br>";
-
-            // File
-            $dir = $CFG->dataroot . '/fellesdata';
-            $path = $dir . '/wsUserCompetence.txt';
-
-            // Clean old data
-            // Save new data
-            unlink($path);
-            $filecompetence = fopen($path,'w');
-            fwrite($filecompetence,$competence);
-            fclose($filecompetence);
-
-            if (file_exists($path)) {
-                // Get content
-                $data = file($path);
-
-                // Synchronization
-                foreach($data as $key=>$line) {
-                    echo "Line: " . $line . "</br>";
-                }
-            }
                     // Call web service
                     // Params web service
                     $params = array();
                     $params['usersCompetence'] = $competence;
 
-                    //$response = self::process_service($plugin,KS_USER_COMPETENCE,$params);
-                    //if ($response['error'] == '200') {
+                    $response = self::process_service($plugin,KS_USER_COMPETENCE,$params);
+                    if ($response['error'] == '200') {
                         // Synchronize user competence
-                    //    FSKS_USERS::synchronize_user_competence_fs($competence,$response['usersCompetence']);
-                    //}else {
+                        FSKS_USERS::synchronize_user_competence_fs($competence,$response['usersCompetence']);
+                    }else {
                         // Log
-                    //    $dbLog  = "ERROR WS: " . $response['message'] . "\n" . "\n";
-                    //    $dbLog .= userdate(time(),'%d.%m.%Y', 99, false). ' Finish ERROR Synchronization STATUS competence . ' . "\n";
-                    //    error_log($dbLog, 3, $CFG->dataroot . "/Fellesdata.log");
-                    //}//if_no_error
+                        $dbLog  = "ERROR WS: " . $response['message'] . "\n" . "\n";
+                        $dbLog .= userdate(time(),'%d.%m.%Y', 99, false). ' Finish ERROR Synchronization STATUS competence . ' . "\n";
+                        error_log($dbLog, 3, $CFG->dataroot . "/Fellesdata.log");
+                    }//if_no_error
              //   }//for_rdo
             //}//if_totla
 
