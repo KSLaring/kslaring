e2f0b0db2de0acafb9d18d384a257fc32046227d
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-03-03 14:18:05 +0100

    Felelsdata

--- local/fellesdata_status/cron/statuscron.php
+++ local/fellesdata_status/cron/statuscron.php
@@ -48,13 +48,13 @@ class STATUS_CRON {
             //self::competence_data($plugin,$industry);
 
             // Get managers reporters from KS
-            self::managers_reporters($plugin,$industry);
+            //self::managers_reporters($plugin,$industry);
 
             // Import last status from fellesdata
             //self::import_status($plugin);
 
             // Syncronization
-            //self::synchronization($plugin);
+            self::synchronization($plugin);
         }catch (Exception $ex) {
             throw $ex;
         }//try_catch
