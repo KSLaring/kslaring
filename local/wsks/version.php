758fc82a73eada9b047f2e79c123de92c588f99a
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-03-05 12:36:31 +0100

    Felledata

--- local/wsks/version.php
+++ local/wsks/version.php
@@ -11,5 +11,5 @@
  */
 
 //$plugin->version    = '2015103000';         /* The plugin current version (DATE: YYYYMMDDXX) */
-$plugin->version    =   '2017030210';
+$plugin->version    =   '2017030212';
 $plugin->component  =   'local_wsks';
