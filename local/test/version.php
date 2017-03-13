e3ee29130eed1e9d16593a7f4b16b074f99ca528
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-03-13 11:59:08 +0100

    Fellesdata status

--- local/test/version.php
+++ local/test/version.php
@@ -8,3 +8,6 @@
 
 $plugin->version    =  '2017022002';         /* The plugin current version (DATE: YYYYMMDDXX) */
 $plugin->component  =  'local_test';
+
+/* Dependencies */
+$plugin->dependencies = array('local_fellesdata'  => 2017022002);
