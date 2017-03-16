b483fec2a2c9c414828b3635a407f6771cfa8fdb
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-03-16 12:04:27 +0100

    my users

---
+++ local/myusers/lang/en/local_myusers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Users Admin - Category plugin  - Language Settings
+ *
+ * @package         local
+ * @subpackage      myusers
+ * @copyright       2014    eFaktor {@link http://www.efaktor.no}
+ *
+ * @creationDate    27/01/2014
+ * @author          eFaktor     (fbv)
+ */
+
+$string['pluginname']   = 'User Administration - Category';
+$string['title']        = '{$a} Users';
+$string['company']      = 'Company';
+$string['job_roles'] = 'Job roles';
---
+++ local/myusers/lang/no/local_myusers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Users Admin - Category plugin  - Language Settings (Norwegian)
+ *
+ * @package         local
+ * @subpackage      myusers
+ * @copyright       2014    eFaktor {@link http://www.efaktor.no}
+ *
+ * @creationDate    27/01/2014
+ * @author          eFaktor     (fbv)
+ */
+
+$string['pluginname']   = 'Kategoribasert brukeradmin';
+$string['title']        = '{$a} brukere';
+$string['company']      = 'Arbeidssted';
+$string['job_roles']        = 'Jobbroller';
