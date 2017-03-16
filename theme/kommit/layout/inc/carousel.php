e029153f370984e6fdd355310d79ae33913aefad
uhunkler <urs.hunkler@unodo.de>
2017-03-16 10:06:22 +0100

    theme_kommit - correct the slideshow dot indicator

--- theme/kommit/layout/inc/carousel.php
+++ theme/kommit/layout/inc/carousel.php
@@ -59,12 +59,15 @@ if ($show && $noslides) {
             <div id="kslCarousel-wrapper">
                 <div class="container-fluid">
                     <div id="kslCarousel" class="carousel slide">
-                        <ol class="carousel-indicators">
-                            <li data-target="#kslCarousel" data-slide-to="0"
-                                class="active"></li>
-                            <li data-target="#kslCarousel" data-slide-to="1"></li>
-                            <li data-target="#kslCarousel" data-slide-to="2"></li>
-                        </ol>
+                        <?php if ($noslides > 1) { ?>
+                            <ol class="carousel-indicators">
+                                <li data-target="#kslCarousel" data-slide-to="0"
+                                    class="active"></li>
+                                <?php for ($i = 2; $i <= $noslides; $i++) { ?>
+                                    <li data-target="#kslCarousel" data-slide-to="<?php echo $i ?>"></li>
+                                <?php } ?>
+                            </ol>
+                        <?php } ?>
                         <!-- Carousel items -->
                         <div class="carousel-inner">
                             <?php for ($i = 1; $i <= $noslides; $i++) {
