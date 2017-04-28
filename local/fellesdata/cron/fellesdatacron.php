c5a627a38f5d213de0606eff26ae39e02f2f8777
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-04-28 13:45:53 +0200

    Fellesdata

--- local/fellesdata/cron/fellesdatacron.php
+++ local/fellesdata/cron/fellesdatacron.php
@@ -647,7 +647,7 @@ class FELLESDATA_CRON {
                             }else {
                                 $content = file($pathFile);
                             }
-
+                            $content = file($pathFile);
                             FS::save_temporary_fellesdata($content,IMP_COMPANIES);
                         }else {
                             // Mark file as suspicious
