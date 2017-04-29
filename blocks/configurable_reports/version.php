c808cedb7c2cd2b49ea9a92910f264d3562da6db
Alf Martin Johnsen <amj@efaktor.no>
2017-04-29 12:33:09 +0200

    Updated configurable reports

--- blocks/configurable_reports/version.php
+++ blocks/configurable_reports/version.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -30,10 +29,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2011040119;  // Plugin version.
-$plugin->requires = 2011120500; // require Moodle version (2.2).
+$plugin->version = 2016020103;  // Plugin version.
+$plugin->requires = 2015111600; // require Moodle version (3.0).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.3.7';
+$plugin->release = '3.1.1';
 $plugin->component = 'block_configurable_reports'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 86400; // = Once in 24h, Set min time between cron executions.
                             // Should probably be at night to off load CPU load.
