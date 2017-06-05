55a720626bc5d911a277dcaeb6d7d79924d0808c
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-06-05 10:44:00 +0200

    Last version

--- local/fellesdata/cron/fellesdatacron.php
+++ local/fellesdata/cron/fellesdatacron.php
@@ -70,23 +70,23 @@ class FELLESDATA_CRON {
             }//fstExecution_tounmap
 
             // Import KS
-            self::import_ks($plugin,$dblog);
+            //self::import_ks($plugin,$dblog);
 
             // Import fellesdata
             self::import_fellesdata($plugin,$dblog);
 
             // Users accounts synchornization
-            self::users_fs_synchronization($plugin,$dblog);
+            //self::users_fs_synchronization($plugin,$dblog);
 
             // Companies synchornization
-            self::companies_fs_synchronization($plugin,$fstExecution,$dblog);
+            //self::companies_fs_synchronization($plugin,$fstExecution,$dblog);
 
             // Job roles to map
-            self::jobroles_fs_to_map($plugin,$dblog);
+            //self::jobroles_fs_to_map($plugin,$dblog);
 
             // Competence synchronization
             if (!$fstExecution) {
-                self::competence_synchronization($plugin,$dblog);
+                //self::competence_synchronization($plugin,$dblog);
             }//if_fstExecution_competence
 
             // Log
