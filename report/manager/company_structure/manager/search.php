86d7de4652db8951574211078156ab2eefa1e470
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-06-08 14:24:18 +0200

    Import competence

--- report/manager/company_structure/manager/search.php
+++ report/manager/company_structure/manager/search.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Report Competence Manager - Managers - Search process
  *
@@ -7,6 +21,7 @@
  * @package         report/manager
  * @subpackage      company_structure/manager
  * @copyright       2010 eFaktor
+ * @license         http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
  * @creationDate    21/12/2015
  * @author          eFaktor     (fbv)
--- report/manager/version.php
+++ report/manager/version.php
@@ -1,5 +1,18 @@
 <?php
-
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Report Competence Manager - Version
  *
@@ -8,6 +21,8 @@
  * @package         report
  * @subpackage      manager
  * @copyright       2010 eFaktor
+ * @license         http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
  * @updateDate      06/09/2012
  * @author          eFaktor     (fbv)
  *
@@ -15,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version    = 2017052400;           /* The current plugin version (Date: YYYYMMDDXX)  */
+$plugin->version    = 2017060800;           /* The current plugin version (Date: YYYYMMDDXX)  */
 $plugin->requires   = 2012061700;           /* Requires this Moodle version                   */
 $plugin->component  = 'report_manager';   /* Full name of the plugin (used for diagnostics) */
 
