db1ebe34a54b1846fb6c0fb75fb74f0f50b64786
Alf Martin Johnsen <amj@efaktor.no>
2017-06-12 21:30:13 +0200

    New translations

---
+++ local/course_search/lang/no/local_course_search.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings
+ *
+ * @package    local
+ * @subpackage course_search
+ * @copyright  2017 eFaktor
+ * @author     Urs Hunkler {@link urs.hunkler@unodo.de}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Kurssøk';
+$string['title'] = 'Kurssøk';
+$string['crontask'] = 'Indeksering av fulltekst søk';
+$string['pagetitle'] = 'Kurssøk';
+$string['searchtitle'] = 'Fritekstsøk';
+$string['searchbtntext'] = 'Søk';
+$string['toggleselection'] = 'Endre interesseområde for søket';
+$string['preselectprovider'] = 'Forvalg av tilbydere';
+$string['preselectothers'] = 'Forvalg av andre';
+$string['provider'] = 'Tilbyder';
+$string['date'] = 'Dato';
+$string['nocoursesshowninfo'] = 'Hvis du ikke ser noen kurs på denne siden må du først velge et interesseområde av ting du er interessert å søke i. Vennligst foreta et utvalg og lagre dette for å få opp relevante treff innen ditt interesseområde.';
