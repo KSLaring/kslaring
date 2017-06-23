f5a2e76d518003f731eb5732ab5c830dbd77f1e1
uhunkler <urs.hunkler@unodo.de>
2017-06-23 09:46:45 +0200

    theme_kommit - correct issue where right column blocks lack left margin

--- theme/kommit/version.php
+++ theme/kommit/version.php
@@ -31,8 +31,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2017061200;
-$plugin->release = '3.1.3 (Build: 2017032100)';
+$plugin->version = 2017062300;
+$plugin->release = '3.1.5 (Build: 2017062300)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2015111602;
 $plugin->component = 'theme_kommit';
