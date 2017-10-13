5e48fa1c9f42bfdffd8ee926d48f5143a1b24251
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-10-13 12:35:42 +0200

    Felelsdata/Status

--- local/fellesdata/mapping/organization.php
+++ local/fellesdata/mapping/organization.php
@@ -89,6 +89,7 @@ if (($level > 1) && (!$parentid)) {
 }else {
     if (isset($SESSION->fsparents)) {
         $fsparents = $SESSION->fsparents[$parentid];
+        $fsparents = str_replace('LE1','',$fsparents);
     }else {
         $fsparents = 0;
     }
