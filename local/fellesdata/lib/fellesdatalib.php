7ed0648dad85218a7f186fcb20f53f47bb948170
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-10-16 10:57:01 +0200

    Felelsdata/Status

--- local/fellesdata/lib/fellesdatalib.php
+++ local/fellesdata/lib/fellesdatalib.php
@@ -3251,6 +3251,7 @@ class FS {
             $sql = " SELECT * FROM {fs_imp_company} WHERE org_nivaa NOT IN ('" . $nivaa . "')";
 
             //Execute
+            echo $sql . "</br>";
             $rdo = $DB->get_records_sql($sql);
             if ($rdo) {
                 $DB->insert_records('fs_imp_middle_parents',$rdo);
