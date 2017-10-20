80289350c8579a362af283b2a52fdb3f6750c2e5
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-10-20 14:54:12 +0200

    Felelsdata/Status

--- local/fellesdata/lib/mappinglib.php
+++ local/fellesdata/lib/mappinglib.php
@@ -715,7 +715,6 @@ class FS_MAPPING {
 
             // SQL Instruction
             $sql = " SELECT     DISTINCT
-                                  fs.id,
                                   fs.stillingskode,
                                   fs.stillingstekst
                      FROM		  {fs_imp_jobroles}	fs
@@ -747,7 +746,7 @@ class FS_MAPPING {
             // Execute
             $sql .= " ORDER BY   fs.stillingskode,fs.stillingstekst ";
 
-            $rdo = $DB->get_records_sql($sql,$params);
+            $rdo = $DB->get_recordset_sql($sql,$params);
             if ($rdo) {
                 foreach ($rdo as $instance) {
                     $fsJobroles[$instance->stillingskode] = $instance->stillingskode . " - " . $instance->stillingstekst;
@@ -789,7 +788,7 @@ class FS_MAPPING {
             $params['job_role'] = $ks_jobrole;
 
             // SQL Instruction
-            $sql = " SELECT   DISTINCT 
+            $sql = " SELECT   DISTINCT
                                 fs.jrcode,
                                 fs.jrname
                      FROM	    {fs_jobroles}	fs
@@ -813,7 +812,7 @@ class FS_MAPPING {
 
             // Execute
             $sql .= " ORDER BY fs.jrcode,fs.jrname ";
-            $rdo = $DB->get_records_sql($sql,$params);
+            $rdo = $DB->get_recordset_sql($sql,$params);
             if ($rdo) {
                 foreach ($rdo as $instance) {
                     $fsJobroles[$instance->jrcode] = $instance->jrcode . " - " . $instance->jrname;
@@ -1055,11 +1054,13 @@ class FS_MAPPING {
             // Map/UnMap FS Job Roles
             foreach ($toMap as $fs) {
                 // Get Info FS Job role
-                $rdo = $DB->get_record('fs_imp_jobroles',array('stillingskode' => $fs));
+                $rdo = $DB->get_records('fs_imp_jobroles',array('stillingskode' => $fs));
                 // Mapping
                 if ($rdo) {
+                    foreach ($rdo as $instance) {
+                        self::$function($instance,$ksJobRole);
+                    }
 
-                    self::$function($rdo,$ksJobRole);
                 }//if_rdo
             }//for_fs_jobrole
 
