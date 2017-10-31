a0d5beff1cf959c1c02f3c9a0b36848157456943
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-10-31 11:32:23 +0100

    Testing

--- local/fellesdata/lib/fellesdatalib.php
+++ local/fellesdata/lib/fellesdatalib.php
@@ -2568,6 +2568,7 @@ class FSKS_USERS {
 
             // Info Account
             if (!$rdoUser) {
+                echo "1" . "</br>";
                 // Create new Account
                 $infoUser = new stdClass();
                 if ($userFS->adfs) {
@@ -2608,8 +2609,10 @@ class FSKS_USERS {
                 case STATUS:
                     // Execute
                     if (!$rdoUser) {
+                        echo "2" . "</br>";
                         $userId = $DB->insert_record('user',$infoUser);
                     }else {
+                        echo "3" . "</br>";
                         // Update
                         $userId = $rdoUser->id;
                         $rdoUser->firstname     = $userFS->firstname;
@@ -2650,6 +2653,7 @@ class FSKS_USERS {
 
             // Synchronized
             if ($sync) {
+                echo "4" . "</br>";
                 $instance = new stdClass();
                 $instance->id           = $fsKey;
                 $instance->imported     = 1;
@@ -2663,6 +2667,7 @@ class FSKS_USERS {
             if ($userFS->ressursnr) {
                 $rdo = $DB->get_record('user_resource_number',array('userid' => $userId));
                 if ($rdo) {
+                    echo "5" . "</br>";
                     // Update
                     $rdo->ressursnr      = $userFS->ressursnr;
                     $rdo->industrycode   = $userFS->industry;
@@ -2670,6 +2675,7 @@ class FSKS_USERS {
                     // Execute
                     $DB->update_record('user_resource_number',$rdo);
                 }else {
+                    echo "6" . "</br>";
                     // Insert
                     $instance = new stdClass();
                     $instance->userid        = $userId;
@@ -2684,6 +2690,7 @@ class FSKS_USERS {
             // Fellesdata account has to be deleted
             // Only one account for user
             if ($rdoFellesdata) {
+                echo "7" . "</br>";
                 // From user
                 $DB->delete_records('user',array('id' => $rdoFellesdata->id,'username' => $rdoFellesdata->username));
 
@@ -2694,6 +2701,8 @@ class FSKS_USERS {
             // Commit
             $trans->allow_commit();
         }catch (Exception $ex) {
+            echo $ex->getTraceAsString() . "</br>" . "</br>";
+            echo $ex->getMessage() . "</br>" . "</br>";
             // Rollback
             $trans->rollback($ex);
 
