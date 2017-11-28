878da163296eb4f0684c6cdc461d5e7065ccb631
Paqui Bellés Vallejo <fbv@efaktor.no>
2017-11-28 10:56:36 +0100

    Course report

--- local/tracker_manager/lib.php
+++ local/tracker_manager/lib.php
@@ -19,7 +19,7 @@ function local_tracker_manager_extend_navigation(global_navigation $navigation) 
             $nodBar = $nodeTracker->add(get_string('outcomes','local_tracker_manager'),new moodle_url('/report/manager/outcome/outcome.php'));
             $nodBar = $nodeTracker->add(get_string('spuser','local_tracker_manager'),new moodle_url('/report/manager/super_user/spuser.php'));
         }else {
-            if (CompetenceManager::is_reporter($USER->id)) {
+            if (CompetenceManager::is_super_user($USER->id)) {
                 $nodBar = $nodeTracker->add(get_string('company_structure','local_tracker_manager'),new moodle_url('/report/manager/company_structure/company_structure.php'));
                 $nodBar = $nodeTracker->add(get_string('job_roles','local_tracker_manager'),new moodle_url('/report/manager/job_role/job_role.php'));
                 if ($isReporter) {
